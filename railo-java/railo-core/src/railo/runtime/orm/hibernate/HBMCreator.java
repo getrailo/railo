03918e195c4a9c8cdaf70a03916c3501fe2efdaa
micstriit <michael@getrailo.org>
2010-09-24 23:19:17 +0200

    cleaned

--- railo-java/railo-core/src/railo/runtime/orm/hibernate/HBMCreator.java
+++ railo-java/railo-core/src/railo/runtime/orm/hibernate/HBMCreator.java
@@ -11,7 +11,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
-import railo.print;
 import railo.commons.lang.StringUtil;
 import railo.runtime.Component;
 import railo.runtime.ComponentPro;
