296e3de0007c6871e05fc5b35056beaa56994042
micstriit <michael@getrailo.org>
2010-10-27 16:46:18 +0200

    added build in function datasourceFlushMetaCache

---
+++ railo-java/railo-core/src/railo/runtime/functions/other/DatasourceFlushMetaCache.java
@@ -0,0 +1,34 @@
+package railo.runtime.functions.other;
+
+import java.util.Map;
+
+import railo.commons.lang.StringUtil;
+import railo.runtime.PageContext;
+import railo.runtime.db.DataSource;
+import railo.runtime.db.DataSourceImpl;
+
+
+public class DatasourceFlushMetaCache {
+
+    public synchronized static boolean call(PageContext pc) {
+    	return call(pc, null);
+	}
+
+    public synchronized static boolean call(PageContext pc,String datasource) {
+    	
+    	DataSource[] sources = pc.getConfig().getDataSources();
+    	DataSourceImpl ds;
+    	boolean has=false;
+    	for(int i=0;i<sources.length;i++){
+    		ds=(DataSourceImpl) sources[i];
+    		if(StringUtil.isEmpty(datasource) || ds.getName().equalsIgnoreCase(datasource.trim())){
+    			Map cache=ds.getProcedureColumnCache();
+    			if(cache!=null) cache.clear();
+    			if(!StringUtil.isEmpty(datasource))return true;
+    			has=true;
+    		}
+    	}
+    	return has;
+	}
+
+}
