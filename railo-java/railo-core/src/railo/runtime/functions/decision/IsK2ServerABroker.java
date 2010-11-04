1ae90f6ed4bdbb60670fbd37924534d88b29d8c1
micstriit <michael@getrailo.org>
2010-11-04 18:26:48 +0100

    add deprecated mark

--- railo-java/railo-core/src/railo/runtime/functions/decision/IsK2ServerABroker.java
+++ railo-java/railo-core/src/railo/runtime/functions/decision/IsK2ServerABroker.java
@@ -5,10 +5,11 @@ package railo.runtime.functions.decision;
 
 import railo.runtime.PageContext;
 import railo.runtime.ext.function.Function;
+import railo.runtime.tag.util.DeprecatedUtil;
 
 public final class IsK2ServerABroker implements Function {
 	public static boolean call(PageContext pc ) {
+		DeprecatedUtil.function(pc,"IsK2ServerABroker");
 		return false;
-	    //throw new ExpressionException("The function [IsK2ServerABroker] is Deprecated.");
 	}
 }
