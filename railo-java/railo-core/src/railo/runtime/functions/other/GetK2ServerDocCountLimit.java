c699d25f328c8ce7d3a2551824705b06668cf0dd
micstriit <michael@getrailo.org>
2010-11-04 18:36:47 +0100

    set to deprecated

--- railo-java/railo-core/src/railo/runtime/functions/other/GetK2ServerDocCountLimit.java
+++ railo-java/railo-core/src/railo/runtime/functions/other/GetK2ServerDocCountLimit.java
@@ -5,10 +5,11 @@ package railo.runtime.functions.other;
 
 import railo.runtime.PageContext;
 import railo.runtime.ext.function.Function;
+import railo.runtime.tag.util.DeprecatedUtil;
 
 public final class GetK2ServerDocCountLimit implements Function {
 	public static double call(PageContext pc ) {
+		DeprecatedUtil.function(pc,"GetK2ServerDocCountLimit");
 		return 0;
-	    //throw new ExpressionException("The function [IsK2ServerABroker] is Deprecated.");
 	}
 }
