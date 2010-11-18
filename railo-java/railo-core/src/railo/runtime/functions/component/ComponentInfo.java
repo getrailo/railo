c46d3d4d4411dc7cd7ac3f4658f1c1ea0d5cf9e8
micstriit <michael@getrailo.org>
2010-11-18 09:42:05 +0100

    set function to deprecated

--- railo-java/railo-core/src/railo/runtime/functions/component/ComponentInfo.java
+++ railo-java/railo-core/src/railo/runtime/functions/component/ComponentInfo.java
@@ -6,11 +6,18 @@ package railo.runtime.functions.component;
 import railo.runtime.Component;
 import railo.runtime.PageContext;
 import railo.runtime.ext.function.Function;
+import railo.runtime.functions.other.GetMetaData;
+import railo.runtime.tag.util.DeprecatedUtil;
 import railo.runtime.type.Struct;
 import railo.runtime.type.StructImpl;
 
+/**
+ * @deprecated use function GetMetaData instead
+ *
+ */
 public final class ComponentInfo implements Function {
 	public static Struct call(PageContext pc , Component component) {
+		DeprecatedUtil.function(pc, "ComponentInfo", "GetMetaData");
 		Struct sct=new StructImpl();
 	    sct.setEL("name",component.getName());
 	    sct.setEL("fullname",component.getCallName());
