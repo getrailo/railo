282fc1fe515bc01299d28304dd71426ee322b5c3
micstriit <michael@getrailo.org>
2011-02-14 20:11:21 +0100

    solved ticket https://issues.jboss.org/browse/RAILO-1189

--- railo-java/railo-core/src/railo/runtime/functions/other/URLEncodedFormat.java
+++ railo-java/railo-core/src/railo/runtime/functions/other/URLEncodedFormat.java
@@ -5,6 +5,7 @@ package railo.runtime.functions.other;
 
 import java.io.UnsupportedEncodingException;
 
+import railo.commons.lang.StringUtil;
 import railo.commons.lang.URLEncoder;
 import railo.runtime.PageContext;
 import railo.runtime.exp.PageException;
@@ -28,7 +29,8 @@ public final class URLEncodedFormat implements Function {
 		
 		try {
 			String enc=java.net.URLEncoder.encode(str, encoding);
-			return enc;
+			return StringUtil.replace(enc, "+", "%20", false);
+			//return enc;
 		} 
 		catch (Throwable t) {
 			try {
@@ -39,18 +41,5 @@ public final class URLEncodedFormat implements Function {
 			}
 		}
 	}
-	
-	
-	/*public static void main(String[] args) throws PageException {
-		print.out("%20%09%2B%25%26%24%2C%5C%7C%2F%3A%3B%3D%3F%40%3C%3E%23%7B%7D%28%29%5B%5D%5E%60%7E%2D%5F%2E%2A%27%E2%88%9A%E2%88%82%E2%88%9A%C2%A7%E2%88%9A%C2%BA%22");
-		print.out(call(null," 	+%&$,\\|/:;=?@<>#{}()[]^`~-_.*'öäü\""));
-		print.out(call(null,"\\"));
-		System.out.print("char("+((int)'ö')+")&");
-		System.out.print("char("+((int)'ä')+")&");
-		System.out.print("char("+((int)'ü')+")&");
-		System.out.print("char("+((int)'é')+")&");
-		System.out.print("char("+((int)'à')+")&");
-		System.out.print("char("+((int)'è')+")&");
-	}*/
 	
 }
