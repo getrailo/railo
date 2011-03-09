8df730864d2ed55096f204001dd439f35ee0228d
micstriit <michael@getrailo.org>
2011-03-09 14:58:30 +0100

    solved ticket https://issues.jboss.org/browse/RAILO-1148

--- railo-java/railo-core/src/railo/runtime/functions/orm/EntityNew.java
+++ railo-java/railo-core/src/railo/runtime/functions/orm/EntityNew.java
@@ -1,15 +1,29 @@
 package railo.runtime.functions.orm;
 
+
+import railo.runtime.Component;
 import railo.runtime.PageContext;
 import railo.runtime.exp.PageException;
 import railo.runtime.orm.ORMSession;
 import railo.runtime.orm.ORMUtil;
+import railo.runtime.type.Collection.Key;
+import railo.runtime.type.Struct;
 
 public class EntityNew {
 
 	public static Object call(PageContext pc, String name) throws PageException {
+		return call(pc, name, null);
+	}
+	
+	public static Object call(PageContext pc, String name,Struct properties) throws PageException {
 		ORMSession session=ORMUtil.getSession(pc);
-		//ORMEngine engine = ORMUtil.getEngine(pc);
-		return session.create(pc,name);
+		if(properties==null)return session.create(pc,name);
+		
+		Component entity = session.create(pc,name);
+		Key[] keys = properties.keys();
+		for(int i=0;i<keys.length;i++){
+			entity.call(pc, "set"+keys[i], new Object[]{properties.get(keys[i])});
+		}
+		return entity;
 	}
 }
