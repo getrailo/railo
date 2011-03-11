462a980527b972ede513952a466d42dcc4cd1cfc
micstriit <michael@getrailo.org>
2011-03-11 14:00:13 +0100

    cleaned imports

--- railo-java/railo-core/src/railo/runtime/orm/ORMConfiguration.java
+++ railo-java/railo-core/src/railo/runtime/orm/ORMConfiguration.java
@@ -21,7 +21,6 @@ import railo.runtime.op.Decision;
 import railo.runtime.type.Array;
 import railo.runtime.type.ArrayImpl;
 import railo.runtime.type.Collection;
-import railo.runtime.type.Collection.Key;
 import railo.runtime.type.KeyImpl;
 import railo.runtime.type.List;
 import railo.runtime.type.Struct;
