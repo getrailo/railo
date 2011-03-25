84a15e41b743533d3a1a158132f5c8009b95eac0
micstriit <michael@getrailo.org>
2011-03-25 14:14:24 +0100

    experimentell implementation of the tag Sprite

---
+++ railo-java/railo-core/src/railo/runtime/tag/Sprite.java
@@ -0,0 +1,179 @@
+package railo.runtime.tag;
+
+import java.io.IOException;
+
+import railo.commons.digest.MD5;
+import railo.commons.io.IOUtil;
+import railo.commons.io.res.Resource;
+import railo.commons.io.res.util.ResourceUtil;
+import railo.commons.lang.StringUtil;
+import railo.runtime.PageContextImpl;
+import railo.runtime.PageSource;
+import railo.runtime.exp.PageException;
+import railo.runtime.ext.tag.TagImpl;
+import railo.runtime.functions.image.ImageNew;
+import railo.runtime.functions.image.ImageWrite;
+import railo.runtime.img.Image;
+import railo.runtime.op.Caster;
+import railo.runtime.type.List;
+
+public final class Sprite extends TagImpl {
+	
+	private String _id;
+	private String _ids;
+	private String _srcs;
+	
+	String src;
+		
+
+	
+	/**
+	* @see javax.servlet.jsp.tagext.Tag#release()
+	*/
+	public void release()	{
+		this._id=null;
+		this._ids=null;
+		this.src=null;
+		this._srcs=null;
+		super.release();
+	}
+	
+	
+	
+	public void set_ids(String _ids){
+		this._ids=_ids;
+	}
+
+	public void set_id(String _id){
+		this._id=_id;
+	}
+
+	public void set_srcs(String _srcs){
+		this._srcs=_srcs;
+	}
+
+	public void setSrc(String src){
+		this.src=src;
+	}
+	
+	/**
+	 * @see javax.servlet.jsp.tagext.Tag#doStartTag()
+	*/
+
+	public int doStartTag() throws PageException	{
+		try {
+			return _doStartTag();
+		} catch (Throwable e) {
+			throw Caster.toPageException(e);
+		}
+	}
+	
+	
+	public int _doStartTag() throws Throwable	{
+		
+		// write out div for single item
+		pageContext.write("<div id=\""+_id+"\"></div>");
+		
+		
+		
+		
+		
+		
+		// handle all items
+		if(!StringUtil.isEmpty(_ids)) {
+			String[] ids=List.listToStringArray(_ids, ',');
+			String[] strSrcs=List.listToStringArray(_srcs, ',');
+			Resource[] srcs=new Resource[strSrcs.length];
+			Image[] images=new Image[strSrcs.length];
+			for(int i=0;i<srcs.length;i++){
+				srcs[i]=ResourceUtil.toResourceExisting(pageContext, strSrcs[i]);
+				images[i] = new Image(srcs[i]);
+			}
+			
+			// TODO use the same resource as for cfimage
+			PageSource ps = pageContext.getCurrentTemplatePageSource();
+			Resource curr = ps.getPhyscalFile();// TODO handle archives
+			Resource dir = curr.getParentResource();
+			Resource cssDir = dir.getRealResource("css");
+			Resource pathdir = cssDir;
+			cssDir.mkdirs();
+			
+			
+			//the base name for the files we are going to create as a css and image
+			String baseRenderedFileName = MD5.getDigestAsString(_ids);
+			Resource cssFileName = cssDir.getRealResource(baseRenderedFileName+".css");
+			Resource imgFileName = pathdir.getRealResource(baseRenderedFileName+"."+ResourceUtil.getExtension(src,""));
+			
+			//if the files don't exist, then we create them, otherwise
+			boolean bCreate = !cssFileName.isFile() || !imgFileName.isFile();
+			
+			
+			//Are we going to create it, let's say no
+			String css = "";
+			if(bCreate){
+				int imgMaxHeight = 0;
+				int imgMaxWidth = 0;
+				Image img;
+				int actualWidth,actualHeight;
+				//Setup the max height and width of the new image. 
+				for(int i=0;i<srcs.length;i++){
+					img = images[i];
+					
+					//set the image original height and width 
+					actualWidth = img.getWidth();;
+					actualHeight = img.getHeight();
+									
+									
+					
+					//Check if there is a height, 
+					imgMaxHeight += actualHeight;
+					if(actualWidth  > imgMaxWidth) imgMaxWidth  =  actualWidth;
+				}
+				
+				//Create the new image (hence we needed to do two of these items)
+				Image spriteImage = (Image) ImageNew.call(pageContext,"", ""+imgMaxWidth,""+imgMaxHeight, "argb");
+				
+				int placedHeight = 0;
+				//Loop again but this time, lets do the copy and paste
+				for(int i=0;i<srcs.length;i++){
+					img = images[i];
+					spriteImage.paste(img,1,placedHeight);
+					
+						css += "#"+ids[i]+" {\n\tbackground: url("+baseRenderedFileName+"."+ResourceUtil.getExtension(strSrcs[i],"")+") 0px -"+placedHeight+"px no-repeat; width:"+img.getWidth()+"px; height:"+img.getHeight()+"px;\n} \n";
+						placedHeight += img.getHeight();
+				}
+				
+				//Now Write the CSS and the Sprite Image
+				
+				ImageWrite.call(pageContext, spriteImage, imgFileName.getAbsolutePath());
+				IOUtil.write(cssFileName, css,"UTF-8",false);
+				
+			}
+
+			
+			//pageContext.write("<style>"+css+"</style>");
+
+			try {
+				((PageContextImpl)pageContext).getRootOut()
+					.appendHTMLHead("<link rel=\"stylesheet\" href=\"css/"+baseRenderedFileName+".css\" type=\"text/css\" media=\"screen\" title=\"no title\" charset=\"utf-8\">");
+			} catch (IOException e) {
+				Caster.toPageException(e);
+			} 
+			
+		}
+		
+		
+		
+		
+		return SKIP_BODY;
+	}
+
+
+
+	/**
+	* @see javax.servlet.jsp.tagext.Tag#doEndTag()
+	*/
+	public int doEndTag()	{
+		return EVAL_PAGE;
+	}
+}
---
+++ railo-java/railo-core/src/railo/transformer/cfml/evaluator/impl/Sprite.java
@@ -0,0 +1,105 @@
+package railo.transformer.cfml.evaluator.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import railo.commons.lang.IDGenerator;
+import railo.commons.lang.Md5;
+import railo.runtime.exp.PageRuntimeException;
+import railo.runtime.op.Caster;
+import railo.transformer.bytecode.Page;
+import railo.transformer.bytecode.expression.Expression;
+import railo.transformer.bytecode.literal.LitString;
+import railo.transformer.bytecode.op.OpString;
+import railo.transformer.bytecode.statement.tag.Attribute;
+import railo.transformer.bytecode.statement.tag.Tag;
+import railo.transformer.bytecode.util.ASMUtil;
+import railo.transformer.cfml.evaluator.EvaluatorException;
+import railo.transformer.cfml.evaluator.EvaluatorSupport;
+import railo.transformer.library.function.FunctionLib;
+import railo.transformer.library.tag.TagLibTag;
+
+
+public final class Sprite extends EvaluatorSupport {
+	
+	private static final Expression DELIMETER = LitString.toExprString(",");
+	private static Map<String,Previous> sprites=new HashMap<String,Previous>(); 
+	
+	
+	
+	
+	/**
+	 *
+	 * @see railo.transformer.cfml.evaluator.EvaluatorSupport#evaluate(railo.transformer.bytecode.statement.tag.Tag, railo.transformer.library.tag.TagLibTag, railo.transformer.library.function.FunctionLib[])
+	 */
+	public void evaluate(Tag tag,TagLibTag tagLibTag,FunctionLib[] flibs) throws EvaluatorException {
+        String id="sprite_"+IDGenerator.intId();
+        try {
+			Page page = ASMUtil.getAncestorPage(tag);
+			String key=Md5.getDigestAsString(Thread.currentThread().getId()+":"+page.getSource());
+			Expression src = tag.getAttribute("src").getValue();
+			
+			
+			// get data from previous sprites
+			Previous previous = sprites.get(key);
+			if(previous!=null) {
+				previous.tag.removeAttribute("_ids");
+				previous.tag.removeAttribute("_srcs");
+				previous.tag=tag;
+			}
+			else {
+				sprites.put(key, previous = new Previous(tag));
+			}
+			
+			previous.ids.add(id);
+			if(previous.src==null)previous.src=src;
+			else {
+				previous.src=OpString.toExprString(previous.src,DELIMETER);
+				previous.src=OpString.toExprString(previous.src,src);
+			}
+			
+			
+			
+			tag.addAttribute(
+					new Attribute(
+							false,
+							"_id",
+							LitString.toExprString(id),
+							"string"
+					));
+			tag.addAttribute(
+					new Attribute(
+							false,
+							"_ids",
+							LitString.toExprString(railo.runtime.type.List.listToList(previous.ids, ",")),
+							"string"
+					));
+
+			tag.addAttribute(
+					new Attribute(
+							false,
+							"_srcs",
+							previous.src,
+							"string"
+					));
+			
+		} 
+        catch (Throwable e) {// TODO handle Excpetion much more precise
+			throw new PageRuntimeException(Caster.toPageException(e));
+		}
+		   
+	}
+	
+	private static class Previous {
+		public Previous(Tag tag) {
+			this.tag=tag;
+		}
+		private List<String> ids=new ArrayList<String>();
+		private Expression src=null;
+		private Tag tag;
+		
+	}
+	
+}
