be48e12cfe0de526705b53e1fb561416f587bced
micstriit <michael@getrailo.org>
2011-04-07 14:31:13 +0200

    solved ticket https://issues.jboss.org/browse/RAILO-1294

--- railo-java/railo-core/src/railo/runtime/functions/dateTime/DatePart.java
+++ railo-java/railo-core/src/railo/runtime/functions/dateTime/DatePart.java
@@ -5,6 +5,7 @@ package railo.runtime.functions.dateTime;
 
 import java.util.TimeZone;
 
+import railo.print;
 import railo.commons.date.TimeZoneUtil;
 import railo.runtime.PageContext;
 import railo.runtime.exp.ExpressionException;
@@ -14,28 +15,24 @@ import railo.runtime.type.dt.DateTime;
 public final class DatePart implements Function {
 	
 	public static double call(PageContext pc , String datepart, DateTime date) throws ExpressionException {
-		return _call(pc, datepart, date, pc.getTimeZone());
+		return call(pc, datepart, date, null);
 	}
 	
 	public static double call(PageContext pc , String datepart, DateTime date,String strTimezone) throws ExpressionException {
-		return _call(pc, datepart, date, strTimezone==null?pc.getTimeZone():TimeZoneUtil.toTimeZone(strTimezone));
-	}
-	
-	private static double _call(PageContext pc , String datepart, DateTime date,TimeZone tz) throws ExpressionException {
 		datepart=datepart.toLowerCase();
 		char first=datepart.length()==1?datepart.charAt(0):(char)0;
 		
-		if(datepart.equals("yyyy")) return Year.call(pc,date);
-		else if(datepart.equals("ww")) return Week.call(pc,date);
-		else if(first=='w') return DayOfWeek.call(pc,date);
-		else if(first=='q') return Quarter.call(pc,date);
-		else if(first=='m') return Month.call(pc,date);
-		else if(first=='y') return DayOfYear.call(pc,date);
-		else if(first=='d') return Day.call(pc,date);
-		else if(first=='h') return Hour.call(pc,date);
-		else if(first=='n') return Minute.call(pc,date);
-		else if(first=='s') return Second.call(pc,date);
-		else if(first=='l') return MilliSecond.call(pc, date);	
+		if(datepart.equals("yyyy")) return Year.call(pc,date,strTimezone);
+		else if(datepart.equals("ww")) return Week.call(pc,date,strTimezone);
+		else if(first=='w') return DayOfWeek.call(pc,date,strTimezone);
+		else if(first=='q') return Quarter.call(pc,date,strTimezone);
+		else if(first=='m') return Month.call(pc,date,strTimezone);
+		else if(first=='y') return DayOfYear.call(pc,date,strTimezone);
+		else if(first=='d') return Day.call(pc,date,strTimezone);
+		else if(first=='h') return Hour.call(pc,date,strTimezone);
+		else if(first=='n') return Minute.call(pc,date,strTimezone);
+		else if(first=='s') return Second.call(pc,date,strTimezone);
+		else if(first=='l') return MilliSecond.call(pc, date,strTimezone);	
 		throw new ExpressionException("invalid datepart type ["+datepart+"] for function datePart");
 	}
 }
