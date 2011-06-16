d57448fbdcc761863942bbc913596194b535bb02
micstriit <michael@getrailo.org>
2011-06-16 15:35:49 +0200

    solved ticket https://issues.jboss.org/browse/RAILO-1394

--- railo-java/railo-core/src/railo/runtime/db/SQLCaster.java
+++ railo-java/railo-core/src/railo/runtime/db/SQLCaster.java
@@ -148,7 +148,8 @@ public final class SQLCaster {
     	return;
     	case Types.CLOB:			
     		try {
-    			if(value instanceof String) {
+    			stat.setClob(parameterIndex,SQLUtil.toClob(stat.getConnection(),value));
+    			/*if(value instanceof String) {
     				try{
     					stat.setString(parameterIndex,Caster.toString(value));
     				}
@@ -157,7 +158,7 @@ public final class SQLCaster {
     				}
     				
     			}
-    			else stat.setClob(parameterIndex,SQLUtil.toClob(stat.getConnection(),value));
+    			else stat.setClob(parameterIndex,SQLUtil.toClob(stat.getConnection(),value));*/
     		}
     		catch(PageException pe) {
     			if(value instanceof String && StringUtil.isEmpty((String)value))
