5229155a6ed2ff162afe59e36022a35671f0949d
micstriit <michael@getrailo.org>
2011-06-30 23:27:29 +0200

    organized imports

--- railo-java/railo-core/src/railo/transformer/library/function/FunctionLibFactory.java
+++ railo-java/railo-core/src/railo/transformer/library/function/FunctionLibFactory.java
@@ -13,7 +13,6 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
-import org.xml.sax.helpers.XMLReaderFactory;
 
 import railo.commons.collections.HashTable;
 import railo.commons.io.IOUtil;
