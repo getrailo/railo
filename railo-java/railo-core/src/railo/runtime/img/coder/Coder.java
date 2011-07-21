4e85935787248e57d4de6e8f92885165b1b541cc
micstriit <michael@getrailo.org>
2011-07-21 11:48:48 +0200

    improved image implementation

---
+++ railo-java/railo-core/src/railo/runtime/img/coder/Coder.java
@@ -0,0 +1,56 @@
+package railo.runtime.img.coder;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import railo.commons.io.res.Resource;
+import railo.commons.lang.SystemOut;
+import railo.runtime.img.JAIUtil;
+public abstract class Coder {
+	
+	private static Coder instance;
+	
+	protected Coder(){}
+	
+	public static Coder getInstance(){
+		
+		if(instance==null){
+			instance = new JRECoder();
+			
+			// try to load Sanselan, does not load when lib not exists
+			try{
+				SanselanCoder sanselan = new SanselanCoder();
+				instance=new DoubleCoder(sanselan, instance);
+				SystemOut.printDate("use Sanselan and JRE Image Coder ");
+			}
+			catch(Throwable t){
+				SystemOut.printDate("use JRE Image Coder ");
+			}
+		} 
+		return instance;
+	}
+	
+
+	/**
+	 * translate a file resource to a buffered image
+	 * @param res
+	 * @return
+	 * @throws IOException
+	 */
+	public abstract BufferedImage toBufferedImage(Resource res,String format) throws IOException;
+
+	/**
+	 * translate a binary array to a buffered image
+	 * @param binary
+	 * @return
+	 * @throws IOException
+	 */
+	public abstract BufferedImage toBufferedImage(byte[] bytes,String format) throws IOException;
+
+	public abstract String[] getWriterFormatNames();
+	
+	public abstract String[] getReaderFormatNames();
+	
+}
---
+++ railo-java/railo-core/src/railo/runtime/img/coder/SanselanCoder.java
@@ -0,0 +1,69 @@
+package railo.runtime.img.coder;
+
+import java.awt.image.BufferedImage;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.sanselan.ImageReadException;
+import org.apache.sanselan.Sanselan;
+
+import railo.commons.io.IOUtil;
+import railo.commons.io.res.Resource;
+import railo.commons.lang.ExceptionUtil;
+
+class SanselanCoder extends Coder {
+	
+	private String[] writerFormatNames=new String[]{"PNG","GIF","TIFF","JPEG","BMP","PNM","PGM","PBM","PPM","XMP"};
+	private String[] readerFormatNames=new String[]{"PNG","GIF","TIFF","JPEG","BMP","PNM","PGM","PBM","PPM","XMP" ,"ICO","PSD"};
+	
+	protected SanselanCoder(){
+		super();
+		Sanselan.hasImageFileExtension("railo.gif");// to make sure Sanselan exist when load this class
+	}
+	
+	/**
+	 * translate a file resource to a buffered image
+	 * @param res
+	 * @return
+	 * @throws IOException
+	 */
+	public final BufferedImage toBufferedImage(Resource res,String format) throws IOException {
+		InputStream is=null;
+		try {
+			return Sanselan.getBufferedImage(is=res.getInputStream());
+		} 
+		catch (ImageReadException e) {
+			throw ExceptionUtil.toIOException(e);
+		}
+		finally {
+			IOUtil.closeEL(is);
+		}
+	}
+
+	/**
+	 * translate a binary array to a buffered image
+	 * @param binary
+	 * @return
+	 * @throws IOException
+	 */
+	public final BufferedImage toBufferedImage(byte[] bytes,String format) throws IOException {
+		try {
+			return Sanselan.getBufferedImage(new ByteArrayInputStream(bytes));
+		} 
+		catch (ImageReadException e) {
+			throw ExceptionUtil.toIOException(e);
+		}
+	}
+
+	@Override
+	public String[] getWriterFormatNames() {
+		return writerFormatNames;
+	}
+
+	@Override
+	public String[] getReaderFormatNames() {
+		return readerFormatNames;
+	}
+}
