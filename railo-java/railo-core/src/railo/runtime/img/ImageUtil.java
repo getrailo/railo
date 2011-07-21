142e0159754d8e17e1bf73bf27441196aeb81446
micstriit <michael@getrailo.org>
2011-07-21 12:22:57 +0200

    add experminetell support for Sanselan Image decoder/encoder (disabled by default)

--- railo-java/railo-core/src/railo/runtime/img/ImageUtil.java
+++ railo-java/railo-core/src/railo/runtime/img/ImageUtil.java
@@ -3,14 +3,8 @@ package railo.runtime.img;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.IndexColorModel;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
-import javax.imageio.ImageIO;
 import javax.imageio.stream.ImageInputStream;
 
 import org.apache.commons.codec.binary.Base64;
@@ -20,11 +14,25 @@ import railo.commons.io.res.Resource;
 import railo.commons.io.res.util.ResourceUtil;
 import railo.commons.lang.StringUtil;
 import railo.runtime.exp.ExpressionException;
+import railo.runtime.img.coder.Coder;
 
 public class ImageUtil {
 	
-	private static String[] writerFormatNames;
-	private static String[] readerFormatNames;
+	private static Coder coder;
+	
+	static {
+		coder = Coder.getInstance();
+	}
+	
+
+
+	public static String[] getWriterFormatNames() {
+		return coder.getWriterFormatNames();
+	}
+	public static String[] getReaderFormatNames() {
+		return coder.getReaderFormatNames();
+	}
+	
 
 	/**
 	 * translate a file resource to a buffered image
@@ -33,35 +41,7 @@ public class ImageUtil {
 	 * @throws IOException
 	 */
 	public static BufferedImage toBufferedImage(Resource res,String format) throws IOException {
-		if(StringUtil.isEmpty(format))format=getFormat(res);
-		if("psd".equalsIgnoreCase(format)) {
-			PSDReader reader = new PSDReader();
-			InputStream is=null;
-			try {
-				reader.read(is=res.getInputStream());
-				return reader.getImage();
-			}
-			finally {
-				IOUtil.closeEL(is);
-			}
-		}
-		if(JAIUtil.isSupportedReadFormat(format)){
-			return JAIUtil.read(res);
-		}
-		
-		BufferedImage img=null;
-		InputStream is=null;
-		try {
-			img = ImageIO.read(is=res.getInputStream());
-		}
-		finally {
-			IOUtil.closeEL(is);
-		}
-		
-		if(img==null && StringUtil.isEmpty(format)) {
-			return JAIUtil.read(res);
-		}
-		return img;
+		return coder.toBufferedImage(res, format);
 	}
 
 	/**
@@ -71,20 +51,12 @@ public class ImageUtil {
 	 * @throws IOException
 	 */
 	public static BufferedImage toBufferedImage(byte[] bytes,String format) throws IOException {
-		if(StringUtil.isEmpty(format))format=getFormat(bytes,null);
-		if("psd".equalsIgnoreCase(format)) {
-			PSDReader reader = new PSDReader();
-			reader.read(new ByteArrayInputStream(bytes));
-			return reader.getImage();
-		}
-		if(JAIUtil.isSupportedReadFormat(format)){
-			return JAIUtil.read(new ByteArrayInputStream(bytes),format);
-		}
-		BufferedImage img = ImageIO.read(new ByteArrayInputStream(bytes));
-		if(img==null && StringUtil.isEmpty(format))
-			return JAIUtil.read(new ByteArrayInputStream(bytes),null);
-		return img;
+		return coder.toBufferedImage(bytes, format);
 	}
+
+	
+	
+	
 
 	public static byte[] readBase64(String b64str) throws ExpressionException {
 		if(StringUtil.isEmpty(b64str))
@@ -127,10 +99,17 @@ public class ImageUtil {
 		if("ico".equalsIgnoreCase(ext))return "bmp";
 		if("wbmg".equalsIgnoreCase(ext))return "wbmg";
 		if("psd".equalsIgnoreCase(ext))return "psd";
-		if("pnm".equalsIgnoreCase(ext))return "pnm";
 		if("fpx".equalsIgnoreCase(ext))return "fpx";
+		
+		if("pnm".equalsIgnoreCase(ext))return "pnm";
+		if("pgm".equalsIgnoreCase(ext))return "pgm";
+		if("pbm".equalsIgnoreCase(ext))return "pbm";
+		if("ppm".equalsIgnoreCase(ext))return "ppm";
 		return defaultValue;	
 	}
+	
+	
+	
 	
 	public static String getFormatFromMimeType(String mt) throws IOException {
 		String format = getFormatFromMimeType(mt, null);
@@ -142,25 +121,86 @@ public class ImageUtil {
 	
 	public static String getFormatFromMimeType(String mt, String defaultValue) {
 		if("image/gif".equals(mt)) return "gif";
+		if("image/gi_".equals(mt)) return "gif";
+		
 		if("image/jpeg".equals(mt)) return "jpg";
 		if("image/jpg".equals(mt)) return "jpg";
 		if("image/jpe".equals(mt)) return "jpg";
 		if("image/pjpeg".equals(mt)) return "jpg";
+		if("image/vnd.swiftview-jpeg".equals(mt)) return "jpg";
+		if("image/pipeg".equals(mt)) return "jpg";
+		if("application/x-jpg".equals(mt)) return "jpg";
+		if("application/jpg".equals(mt)) return "jpg";
+		if("image/jp_".equals(mt)) return "jpg";
+		
 		if("image/png".equals(mt)) return "png";
 		if("image/x-png".equals(mt))return "png";
+		if("application/x-png".equals(mt)) return "png";
+		if("application/png".equals(mt)) return "png";
+		
 		if("image/tiff".equals(mt)) return "tiff";
+		if("image/tif".equals(mt)) return "tiff";
+		if("image/x-tif".equals(mt)) return "tiff";
+		if("image/x-tiff".equals(mt)) return "tiff";
+		if("application/tif".equals(mt)) return "tiff";
+		if("application/x-tif".equals(mt)) return "tiff";
+		if("application/tiff".equals(mt)) return "tiff";
+		if("application/x-tiff".equals(mt)) return "tiff";
+		
 		if("image/bmg".equals(mt)) return "bmg";
-		if("image/pnm".equals(mt)) return "pnm";
-		if("image/x-portable-anymap".equals(mt)) return "pnm";
 		if("image/vnd.wap.wbmp".equals(mt)) return "wbmg";
+		
 		if("image/fpx".equals(mt)) return "fpx";
-		if("application/vnd.fpx".equals(mt)) return "fpx";
-		if("application/vnd.netfpx".equals(mt)) return "fpx";
 		if("image/x-fpx".equals(mt)) return "fpx";
 		if("image/vnd.fpx".equals(mt)) return "fpx";
+		if("image/vnd.netfpx".equals(mt)) return "fpx";
+		if("image/vnd.fpx".equals(mt)) return "fpx";
+		if("application/vnd.netfpx".equals(mt)) return "fpx";
+		if("application/vnd.fpx".equals(mt)) return "fpx";
 		
+		if("image/x-portable-anymap".equals(mt)) return "pnm";
+		if("image/x-portable/anymap".equals(mt)) return "pnm";
+		if("image/x-pnm".equals(mt)) return "pnm";
+		if("image/pnm".equals(mt)) return "pnm";
+		
+		if("image/x-portable-graymap".equals(mt)) return "pgm";
+		if("image/x-portable/graymap".equals(mt)) return "pgm";
+		if("image/x-pgm".equals(mt)) return "pgm";
+		if("image/pgm".equals(mt)) return "pgm";
+		
+		if("image/portable bitmap".equals(mt)) return "pbm";
+		if("image/x-portable-bitmap".equals(mt)) return "pbm";
+		if("image/x-portable/bitmap".equals(mt)) return "pbm";
+		if("image/x-pbm".equals(mt)) return "pbm";
+		if("image/pbm".equals(mt)) return "pbm";
+		
+		if("image/x-portable-pixmap".equals(mt)) return "ppm";
+		if("application/ppm".equals(mt)) return "ppm";
+		if("application/x-ppm".equals(mt)) return "ppm";
+		if("image/x-p".equals(mt)) return "ppm";
+		if("image/x-ppm".equals(mt)) return "ppm";
+		if("image/ppm".equals(mt)) return "ppm";
+
+		if("image/ico".equals(mt)) return "ico";
+		if("image/x-icon".equals(mt)) return "ico";
+		if("application/ico".equals(mt)) return "ico";
+		if("application/x-ico".equals(mt)) return "ico";
+
+		if("image/photoshop".equals(mt)) return "psd";
+		if("image/x-photoshop".equals(mt)) return "psd";
+		if("image/psd".equals(mt)) return "psd";
+		if("application/photoshop".equals(mt)) return "psd";
+		if("application/psd".equals(mt)) return "psd";
+		if("zz-application/zz-winassoc-psd".equals(mt)) return "psd";
+		
+		// can not terminate this types exactly
+		// image/x-xbitmap
+		// application/x-win-bitmap
+		// image/x-win-bitmap
+		// application/octet-stream
 		return defaultValue;
 	}
+	
 	
 	public static String getMimeTypeFromFormat(String mt) throws IOException {
 		if("gif".equals(mt)) return "image/gif";
@@ -169,11 +209,21 @@ public class ImageUtil {
 		if("jpe".equals(mt)) return "image/jpg";
 		if("png".equals(mt)) return "image/x-png";
 		if("tiff".equals(mt)) return "image/tiff";
-		if("bmg".equals(mt)) return "image/bmg";
-		if("pnm".equals(mt)) return "image/pnm";
+		if("tif".equals(mt)) return "image/tiff";
+		if("bmg".equals(mt)) return "image/bmp";
+		if("bmp".equals(mt)) return "image/bmp";
 		if("wbmg".equals(mt)) return "image/vnd.wap.wbmp";
+		if("wbmp".equals(mt)) return "image/vnd.wap.wbmp";
 		if("fpx".equals(mt)) return "image/fpx";
+		
+		if("pgm".equals(mt)) return "image/x-portable-graymap";
+		if("pnm".equals(mt)) return "image/x-portable-anymap";
+		if("pbm".equals(mt)) return "image/x-portable-bitmap";
+		if("ppm".equals(mt)) return "image/x-portable-pixmap";
 
+		if("ico".equals(mt)) return "image/ico";
+		if("psd".equals(mt)) return "image/psd";
+		
 		if(StringUtil.isEmpty(mt))throw new IOException("can't find Format of given image");//31
 		throw new IOException("can't find Format ("+mt+") of given image");
 	}
@@ -185,38 +235,6 @@ public class ImageUtil {
     	 catch (Throwable t) {}
 		
 		
-	}
-
-	public static String[] getWriterFormatNames() {
-		if(writerFormatNames==null)	{
-			String[] iio = ImageIO.getWriterFormatNames();
-			String[] jai = JAIUtil.isJAISupported()?JAIUtil.getSupportedWriteFormat():null;
-			writerFormatNames=_getFormatNames(iio,jai);
-		}
-		return writerFormatNames;
-	}
-	public static String[] getReaderFormatNames() {
-		if(readerFormatNames==null){
-			String[] iio = ImageIO.getReaderFormatNames();
-			String[] jai = JAIUtil.isJAISupported()?JAIUtil.getSupportedReadFormat():null;
-			readerFormatNames=_getFormatNames(iio,jai);
-		}
-		return readerFormatNames;
-	}
-	
-	private static String[] _getFormatNames(String[] names1,String[] names2) {
-		Set<String> set=new HashSet<String>();
-		
-		if(names1!=null)for(int i=0;i<names1.length;i++){
-			set.add(names1[i].toLowerCase());
-		}
-		if(names2!=null)for(int i=0;i<names2.length;i++){
-			set.add(names2[i].toLowerCase());
-		}
-		
-		names1= set.toArray(new String[set.size()]);
-		Arrays.sort(names1);
-		return names1;
 	}
 
 	public static BufferedImage createBufferedImage(BufferedImage image, int columns, int rows) {
