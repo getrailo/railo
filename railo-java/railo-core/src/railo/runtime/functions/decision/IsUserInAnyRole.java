63ee889b9221695c01db54d5b38ac403f0bb6d20
micstriit <michael@getrailo.org>
2011-09-15 20:32:32 +0200

    solved ticket https://issues.jboss.org/browse/RAILO-571

--- railo-java/railo-core/src/railo/runtime/functions/decision/IsUserInAnyRole.java
+++ railo-java/railo-core/src/railo/runtime/functions/decision/IsUserInAnyRole.java
@@ -3,15 +3,28 @@
  */
 package railo.runtime.functions.decision;
 
+import railo.commons.lang.StringUtil;
 import railo.runtime.PageContext;
 import railo.runtime.exp.PageException;
 import railo.runtime.ext.function.Function;
 import railo.runtime.security.Credential;
+import railo.runtime.type.List;
 
 public final class IsUserInAnyRole implements Function {
 	public static boolean call(PageContext pc) throws PageException {
-		Credential ru = pc.getRemoteUser();
-	    if(ru==null) return false;
-	    return ru.getRoles().length>0;
+		return call(pc, null);
+	}
+	public static boolean call(PageContext pc, String strRoles) throws PageException {
+		if(StringUtil.isEmpty(strRoles)){
+			Credential ru = pc.getRemoteUser();
+			if(ru==null) return false;
+		    return ru.getRoles().length>0;
+		}
+		
+		String[] roles = List.trimItems(List.listToStringArray(strRoles, ','));
+		for(int i=0;i<roles.length;i++) {
+			if(IsUserInRole.call(pc, roles[i])) return true;
+		}
+		return false;
 	}
 }
