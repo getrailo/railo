3798a7f7d90e0b64fc690153ce4014b2af0c7ec0
micstriit <michael@getrailo.org>
2011-11-02 09:57:08 +0100

    solved ticket https://issues.jboss.org/browse/RAILO-1598

--- railo-java/railo-core/src/railo/runtime/functions/list/ListToArray.java
+++ railo-java/railo-core/src/railo/runtime/functions/list/ListToArray.java
@@ -3,6 +3,7 @@
  */
 package railo.runtime.functions.list;
 
+import railo.print;
 import railo.runtime.PageContext;
 import railo.runtime.ext.function.Function;
 import railo.runtime.type.Array;
@@ -27,12 +28,17 @@ public final class ListToArray implements Function {
 	
 
 	public static Array call(PageContext pc , String list, String delimeter,boolean includeEmptyFields,boolean multiCharacterDelimiter) {
+		if(includeEmptyFields){
+			if(list.length()==0) {
+				Array a=new ArrayImpl();
+				a.appendEL("");
+				return a;
+			}
+			return List.listToArray(list,delimeter,multiCharacterDelimiter);
+		}
 		if(list.length()==0) 
 			return new ArrayImpl();
-		if(includeEmptyFields)return List.listToArray(list,delimeter,multiCharacterDelimiter);
+		
 		return List.listToArrayRemoveEmpty(list,delimeter,multiCharacterDelimiter);
 	}
-	
-	
-	
 }
