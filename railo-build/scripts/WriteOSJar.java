9c3de8bcd163ce79e81ecf89a0c77d7ac99a0366
Mark Drew <mark.drew@gmail.com>
2011-12-21 14:28:22 +0000

    new build file and the previous build scripts

---
+++ railo-build/scripts/WriteOSJar.java
@@ -0,0 +1,9 @@
+
+public class WriteOSJar {
+	
+	public static void main(String[] args) throws Exception {
+		WriteOSJar_1.main(args);
+		WriteOSJar_2.main(args);
+		System.out.println("done!");
+	}
+}
