f0c3eb3d4c299e990b494ff59e69d5f6838e6303
micstriit <michael@getrailo.org>
2011-12-23 10:21:03 +0100

    improved error message for LockException

--- railo-java/railo-loader/src/railo/runtime/lock/LockTimeoutException.java
+++ railo-java/railo-loader/src/railo/runtime/lock/LockTimeoutException.java
@@ -13,10 +13,18 @@ public final class LockTimeoutException extends Exception {
 	 */
 	public LockTimeoutException(int type, String name, int timeout) {
 	    //A timeout occurred while attempting to lock lockname
-		super("a timeout occurred on a "+toString(type)+" lock with name ["+name+"] after "+(timeout/1000)+" seconds");
+		super("a timeout occurred on a "+toString(type)+" lock with name ["+name+"] after "+getTime(timeout));
 	}
 
-    private static String toString(int type) {
+    private static String getTime(int timeout) {
+		if(timeout/1000*1000==timeout) {
+			int s = timeout/1000;
+			return s+(s>1?" seconds":" second");
+		}
+		return timeout+(timeout>1?" milliseconds":" millisecond");
+	}
+
+	private static String toString(int type) {
         if(LockManager.TYPE_EXCLUSIVE==type)return "exclusive";
         return "read-only";
     }
