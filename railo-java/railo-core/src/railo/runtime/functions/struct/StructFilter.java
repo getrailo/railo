09035e1e30b048a60e257a41c9955944714aaf57
micstriit <michael@getrailo.org>
2012-01-25 09:46:29 +0100

    solved ticket https://issues.jboss.org/browse/RAILO-1722

---
+++ railo-java/railo-core/src/railo/runtime/functions/struct/StructFilter.java
@@ -0,0 +1,49 @@
+/**
+ * Implements the Cold Fusion Function arrayavg
+ */
+package railo.runtime.functions.struct;
+
+import railo.commons.lang.CFTypes;
+import railo.runtime.PageContext;
+import railo.runtime.exp.ExpressionException;
+import railo.runtime.exp.PageException;
+import railo.runtime.ext.function.Function;
+import railo.runtime.op.Caster;
+import railo.runtime.type.Array;
+import railo.runtime.type.ArrayImpl;
+import railo.runtime.type.Collection.Key;
+import railo.runtime.type.FunctionArgument;
+import railo.runtime.type.Struct;
+import railo.runtime.type.StructImpl;
+import railo.runtime.type.UDF;
+
+
+public final class StructFilter implements Function {
+
+	private static final long serialVersionUID = -91410716194244194L;
+
+	public static Struct call(PageContext pc , Struct sct, UDF filter) throws PageException {
+		
+
+		// check UDF return type
+		int type = filter.getReturnType();
+		if(type!=CFTypes.TYPE_BOOLEAN && type!=CFTypes.TYPE_ANY)
+			throw new ExpressionException("invalid return type ["+filter.getReturnTypeAsString()+"] for UDF Filter, valid return types are [boolean,any]");
+		
+		// check UDF arguments
+		FunctionArgument[] args = filter.getFunctionArguments();
+		if(args.length>2)
+			throw new ExpressionException("UDF filter has to many arguments ["+args.length+"], should have at maximum 2 arguments");
+		
+		
+		Struct rtn=new StructImpl();
+		Key[] keys = sct.keys();
+		Object value;
+		for(int i=0;i<keys.length;i++){
+			value=sct.get(keys[i]);
+			if(Caster.toBooleanValue(filter.call(pc, new Object[]{keys[i],value}, true)))
+				rtn.set(keys[i], value);
+		}
+		return rtn;
+	}
+}
