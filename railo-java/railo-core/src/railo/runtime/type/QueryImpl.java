0a63118b6976350d64e0b8da827d81f3582f339e
micstriit <michael@getrailo.org>
2012-02-02 18:36:09 +0100

    solved ticket https://issues.jboss.org/browse/RAILO-1739

--- railo-java/railo-core/src/railo/runtime/type/QueryImpl.java
+++ railo-java/railo-core/src/railo/runtime/type/QueryImpl.java
@@ -3463,6 +3463,10 @@ public class QueryImpl implements QueryPro,Objects,Sizeable {
 		throw notSupported();
 	}
 	
+	public void removeRows(int index, int count) throws PageException {
+		QueryUtil.removeRows(this,index,count);
+	}
+	
 
 	private SQLException notSupported() {
 		return new SQLException("this feature is not supported");
