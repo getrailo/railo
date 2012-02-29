6a550d18b19c65390c405e8dba707ae516e10b7a
micstriit <michael@getrailo.org>
2012-02-29 16:49:28 +0100

    rename ApplicationGetMetadata to getApplicationMetaData

--- railo-java/railo-core/src/railo/runtime/functions/system/GetApplicationMetadata.java
+++ railo-java/railo-core/src/railo/runtime/functions/system/GetApplicationMetadata.java
@@ -3,7 +3,7 @@ package railo.runtime.functions.system;
 import railo.runtime.PageContext;
 import railo.runtime.type.Struct;
 
-public class ApplicationGetMetaData {
+public class GetApplicationMetadata {
 	public static Struct call(PageContext pc) {
 		return GetApplicationSettings.call(pc, true);
 	}
