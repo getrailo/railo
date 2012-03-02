914aef85a47e8313ace75fd49e6bdc724c9b8596
micstriit <michael@getrailo.org>
2012-03-02 13:41:57 +0100

    solved ticket https://issues.jboss.org/browse/RAILO-1770

--- railo-java/railo-core/src/railo/runtime/type/QueryImpl.java
+++ railo-java/railo-core/src/railo/runtime/type/QueryImpl.java
@@ -29,8 +29,10 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 import railo.commons.db.DBUtil;
 import railo.commons.io.IOUtil;
@@ -471,7 +473,7 @@ public class QueryImpl implements QueryPro,Objects,Sizeable {
 	 * @param rowNumber count of rows to generate (empty fields)
 	 * @param name 
 	 */
-	public QueryImpl(Collection.Key[] columnKeys, int rowNumber,String name) {
+	public QueryImpl(Collection.Key[] columnKeys, int rowNumber,String name) throws DatabaseException {
 		this.name=name;
         columncount=columnKeys.length;
 		recordcount=rowNumber;
@@ -481,6 +483,7 @@ public class QueryImpl implements QueryPro,Objects,Sizeable {
 			columnNames[i]=columnKeys[i];
 			columns[i]=new QueryColumnImpl(this,columnNames[i],Types.OTHER,recordcount);
 		}
+		validateColumnNames(columnNames);
 	}
 	
 	/**
@@ -522,6 +525,7 @@ public class QueryImpl implements QueryPro,Objects,Sizeable {
 		for(int i=0;i<columnNames.length;i++) {
 			columns[i]=new QueryColumnImpl(this,columnNames[i],SQLCaster.toIntType(strTypes[i]),recordcount);
 		}
+		validateColumnNames(columnNames);
 	}
 	
 	/**
@@ -529,8 +533,9 @@ public class QueryImpl implements QueryPro,Objects,Sizeable {
 	 * @param arrColumns columns for the resultset
 	 * @param rowNumber count of rows to generate (empty fields)
 	 * @param name 
+	 * @throws DatabaseException 
 	 */
-	public QueryImpl(Array arrColumns, int rowNumber, String name) {
+	public QueryImpl(Array arrColumns, int rowNumber, String name) throws DatabaseException {
         this.name=name;
         columncount=arrColumns.size();
 		recordcount=rowNumber;
@@ -540,8 +545,9 @@ public class QueryImpl implements QueryPro,Objects,Sizeable {
 			columnNames[i]=KeyImpl.init(arrColumns.get(i+1,"").toString().trim());
 			columns[i]=new QueryColumnImpl(this,columnNames[i],Types.OTHER,recordcount);
 		}
+		validateColumnNames(columnNames);
 	}
-	
+
 	/**
 	 * constructor of the class, to generate a empty resultset (no database execution)
 	 * @param arrColumns columns for the resultset
@@ -561,6 +567,7 @@ public class QueryImpl implements QueryPro,Objects,Sizeable {
 			columnNames[i]=KeyImpl.init(arrColumns.get(i+1,"").toString().trim());
 			columns[i]=new QueryColumnImpl(this,columnNames[i],SQLCaster.toIntType(Caster.toString(arrTypes.get(i+1,""))),recordcount);
 		}
+		validateColumnNames(columnNames);
 	}
 
 	/**
@@ -573,7 +580,18 @@ public class QueryImpl implements QueryPro,Objects,Sizeable {
 
 	public QueryImpl(String[] strColumnNames, Array[] arrColumns, String name) throws DatabaseException {
 		this(_toKeys(strColumnNames),arrColumns,name);		
-		
+	}	
+	
+	private static void validateColumnNames(Key[] columnNames) throws DatabaseException {
+		Set<String> testMap=new HashSet<String>();
+		for(int i=0	;i<columnNames.length;i++) {
+			
+			if(!Decision.isSimpleVariableName(columnNames[i]))
+				throw new DatabaseException("invalid column name ["+columnNames[i]+"] for query", "column names must start with a letter and can be followed by letters numbers and underscores [_]. RegExp:[a-zA-Z][a-zA-Z0-9_]*",null,null,null);
+			if(testMap.contains(columnNames[i].getLowerString()))
+				throw new DatabaseException("invalid parameter for query, ambiguous column name "+columnNames[i],"columnNames: "+List.arrayToListTrim( _toStringKeys(columnNames),","),null,null,null);
+			testMap.add(columnNames[i].getLowerString());
+		}
 	}
 	
 
@@ -621,15 +639,7 @@ public class QueryImpl implements QueryPro,Objects,Sizeable {
 				columns[i]=new QueryColumnImpl(this,columnNames[i],arrColumns[i],Types.OTHER);
 			}
 		// test keys
-			Map testMap=new HashMap();
-			for(int i=0	;i<columnNames.length;i++) {
-				
-				if(!Decision.isSimpleVariableName(columnNames[i]))
-					throw new DatabaseException("invalid column name ["+columnNames[i]+"] for query", "column names must start with a letter and can be followed by letters numbers and underscores [_]. RegExp:[a-zA-Z][a-zA-Z0-9_]*",null,null,null);
-				if(testMap.containsKey(columnNames[i].getLowerString()))
-					throw new DatabaseException("invalid parameter for query, ambiguous column name "+columnNames[i],"columnNames: "+List.arrayToListTrim( _toStringKeys(columnNames),","),null,null,null);
-				testMap.put(columnNames[i].getLowerString(),"set");
-			}
+			validateColumnNames(columnNames);
 		}
 		
 		columncount=columns.length;
@@ -642,8 +652,9 @@ public class QueryImpl implements QueryPro,Objects,Sizeable {
      * @param columnList
      * @param data
      * @param name 
+     * @throws DatabaseException 
      */
-    public QueryImpl(String[] strColumnList, Object[][] data,String name) {
+    public QueryImpl(String[] strColumnList, Object[][] data,String name) throws DatabaseException {
     	
         this(toCollKeyArr(strColumnList),data.length,name);
         
