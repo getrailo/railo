4e5fec70dc9fdbc710f0ce0455f526e2129ce43a
micstriit <michael@getrailo.org>
2012-03-23 09:47:40 +0100

    add function empty

--- railo-java/railo-core/src/railo/runtime/functions/system/Empty.java
+++ railo-java/railo-core/src/railo/runtime/functions/system/Empty.java
@@ -1,28 +1,26 @@
 package railo.runtime.functions.system;
 
-import java.util.Map;
-
-import com.lowagie.text.List;
-
 import railo.runtime.PageContext;
+import railo.runtime.exp.FunctionException;
 import railo.runtime.ext.function.Function;
+import railo.runtime.functions.string.Len;
 import railo.runtime.interpreter.VariableInterpreter;
-import railo.runtime.op.Castable;
 import railo.runtime.op.Caster;
-import railo.runtime.op.Decision;
-import railo.runtime.type.Collection;
-import railo.runtime.type.Query;
 
 public class Empty implements Function {
 
 	private static final long serialVersionUID = 3780957672985941192L;
 	
 
-	public static boolean call(PageContext pc , String variableName) {
+	public static boolean call(PageContext pc , String variableName) throws FunctionException {
 		Object res = VariableInterpreter.getVariableEL(pc, variableName,null);
 		
 		if(res==null) return true;
-		if(res instanceof Number) return ((Number)res).doubleValue()==0.0d;
+		double len=Len.invoke(res, -1);
+		if(len==-1)throw new FunctionException(pc,"empty",1,"variable","this type  ["+Caster.toTypeName(res)+"] is not supported");
+		return len==0;
+		
+		/*if(res instanceof Number) return ((Number)res).doubleValue()==0.0d;
 		if(res instanceof String) {
 			String str=(String) res;
 			if(str.length()==0)return true;
@@ -49,6 +47,6 @@ public class Empty implements Function {
 		
 		if(res instanceof Castable) return call(pc,((Castable)res).castToString("1"));
 		
-		return false;
+		return false;*/
 	}
 }
