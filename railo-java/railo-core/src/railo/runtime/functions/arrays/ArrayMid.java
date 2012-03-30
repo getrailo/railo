3e5479e5956b64bb1d196187e856708d2b7e0e9b
micstriit <michael@getrailo.org>
2012-03-30 15:35:13 +0200

    solved ticket https://issues.jboss.org/browse/RAILO-1808

---
+++ railo-java/railo-core/src/railo/runtime/functions/arrays/ArrayMid.java
@@ -0,0 +1,43 @@
+package railo.runtime.functions.arrays;
+
+import railo.runtime.PageContext;
+import railo.runtime.exp.ExpressionException;
+import railo.runtime.exp.FunctionException;
+import railo.runtime.ext.function.Function;
+import railo.runtime.type.Array;
+import railo.runtime.type.ArrayImpl;
+
+public final class ArrayMid implements Function {
+
+	private static final long serialVersionUID = 4996354700884413289L;
+
+	public static Array call(PageContext pc , Array arr, double start) throws ExpressionException {
+		return call(pc,arr,start,-1);
+	}
+	
+	public static Array call(PageContext pc , Array arr, double start, double count) throws ExpressionException {
+		int s=(int) start;
+		int c=(int) count;
+		
+		if(s<1) throw new FunctionException(pc, "ArrayMid", 2, "start", "Parameter which is now ["+1+"] must be a positive integer");
+		if(c==-1) c=arr.size();
+		else if(c<-1) throw new FunctionException(pc, "ArrayMid", 3, "count", "Parameter which is now ["+c+"] must be a non-negative integer or -1 (for string length)");
+		c+=s-1;
+		if(s>arr.size()) return new ArrayImpl();
+		else if(c>=arr.size())return mid(arr,s,c);
+		else {
+			return mid(arr,s,c);
+		}
+	}
+
+	private static Array mid(Array arr,int start, int count) {
+		ArrayImpl rtn = new ArrayImpl();
+		int len = arr.size();
+		Object value;
+		for(int i=start;i<=count && i<=len ;i++){
+			value=arr.get(i, null);
+			rtn.appendEL(value);
+		}
+		return rtn;
+	}
+}
