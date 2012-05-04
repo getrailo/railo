d613c7d693c426f25f495d9179caa1c1bcfe6ea4
micstriit <michael@getrailo.org>
2012-05-04 21:55:22 +0200

    replaced deprecated method call

--- railo-java/railo-core/src/railo/runtime/tag/StoredProc.java
+++ railo-java/railo-core/src/railo/runtime/tag/StoredProc.java
@@ -545,7 +545,7 @@ public class StoredProc extends BodyTagTryCatchFinallySupport {
 		    res.set(QueryImpl.CACHED,Caster.toBoolean(isFromCache));
 		    
 		    if(pageContext.getConfig().debug() && debug) {
-		    	pageContext.getDebugger().addQueryExecutionTime(datasource,procedure,_sql,count,pageContext.getCurrentPageSource(),(int)exe);
+		    	pageContext.getDebugger().addQuery(null,datasource,procedure,_sql,count,pageContext.getCurrentPageSource(),(int)exe);
 			}
 		    
 		    
