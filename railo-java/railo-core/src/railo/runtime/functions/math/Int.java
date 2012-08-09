ab50d25d613517eeccf2383f1813c1dd86ce319b
Michael Offner <michael@getrailo.com>
2012-08-09 11:22:37 +0200

    solved ticket https://issues.jboss.org/browse/RAILO-2021

--- railo-java/railo-core/src/railo/runtime/functions/math/Int.java
+++ railo-java/railo-core/src/railo/runtime/functions/math/Int.java
@@ -8,6 +8,6 @@ import railo.runtime.ext.function.Function;
 
 public final class Int implements Function {
 	public static double call(PageContext pc , double number) {
-		return (int)number;
+		return Math.floor(number);
 	}
 }
