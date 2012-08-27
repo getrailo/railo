a867ceb0db914f20dd7160c1dd2a4d3157fa7195
Michael Offner <michael@getrailo.com>
2012-08-27 16:06:56 +0200

    solved ticket https://issues.jboss.org/browse/RAILO-2045

--- railo-java/railo-core/src/railo/runtime/functions/arrays/ArrayContainsNoCase.java
+++ railo-java/railo-core/src/railo/runtime/functions/arrays/ArrayContainsNoCase.java
@@ -4,7 +4,9 @@
 package railo.runtime.functions.arrays;
 
 import railo.runtime.PageContext;
+import railo.runtime.exp.PageException;
 import railo.runtime.ext.function.Function;
+import railo.runtime.op.Caster;
 import railo.runtime.type.Array;
 import railo.runtime.type.util.ArrayUtil;
 
@@ -12,7 +14,10 @@ public final class ArrayContainsNoCase implements Function {
 
 	private static final long serialVersionUID = 4394078979692450076L;
 
-	public static double call(PageContext pc , Array array, String value) {
-		return ArrayUtil.arrayContainsIgnoreEmpty(array,value,true)+1;
+	public static double call(PageContext pc , Array array, String value) throws PageException {
+		String str=Caster.toString(value,null);
+		if(str!=null) 
+			return ArrayUtil.arrayContainsIgnoreEmpty(array,value,true)+1;
+		return ArrayFind.call(pc, array, value);
 	}
 }
