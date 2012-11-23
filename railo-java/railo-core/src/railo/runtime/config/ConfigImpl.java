ef5280152242f60e50a459cd88a3adc4111ad70c
micstriit <michael@getrailo.com>
2012-11-23 12:14:42 +0100

    removed unnecessary comment

--- railo-java/railo-core/src/railo/runtime/config/ConfigImpl.java
+++ railo-java/railo-core/src/railo/runtime/config/ConfigImpl.java
@@ -1200,7 +1200,7 @@ public abstract class ConfigImpl implements Config {
     }
     
     public void createTag(TagLib tl,String filename) {// Jira 1298
-    	String name=toName(filename);//filename.substring(0,filename.length()-(getCFCExtension().length()+1));xxx
+    	String name=toName(filename);//filename.substring(0,filename.length()-(getCFCExtension().length()+1));
         
     	TagLibTag tlt = new TagLibTag(tl);
         tlt.setName(name);
