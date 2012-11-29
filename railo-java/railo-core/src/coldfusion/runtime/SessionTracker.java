38333441d9a58b54f53228c9d8c8706708a4cbc7
micstriit <michael@getrailo.com>
2012-11-29 10:54:08 +0100

    extended Sesextended session tracker a bit

--- railo-java/railo-core/src/coldfusion/runtime/SessionTracker.java
+++ railo-java/railo-core/src/coldfusion/runtime/SessionTracker.java
@@ -1,12 +1,39 @@
 package coldfusion.runtime;
 
+import java.io.IOException;
+
+import org.xml.sax.SAXException;
+
 import railo.runtime.CFMLFactoryImpl;
 import railo.runtime.PageContext;
+import railo.runtime.config.ConfigWebAdmin;
+import railo.runtime.config.ConfigWebImpl;
 import railo.runtime.engine.ThreadLocalPageContext;
 import railo.runtime.type.Struct;
 import railo.runtime.type.scope.ScopeContext;
 
 public class SessionTracker {
+
+	/*
+	 * prepare to restrict access to this class
+	 * private String webAdminPassword;
+
+	public SessionTracker(){
+		throw new RuntimeException("for Security reasons we have restricted the usage of the class SessionTracker, " +
+				"you now can only use this class by defining the web admin password, when you call the constructor of this class, " +
+				"so instead of just doing the following: SessionTracker=createObject('java','coldfusion.runtime.SessionTracker') " +
+				"and then use it, you have to call the constructor with the web admin password: SessionTracker=createObject('java','coldfusion.runtime.SessionTracker').init('yourwebadminpassword')");
+		
+		
+	}
+	public SessionTracker(String webAdminPassword) throws SAXException, IOException{
+		this.webAdminPassword=webAdminPassword;
+		
+		PageContext pc = ThreadLocalPageContext.get();
+		//ConfigWebAdmin admin = ConfigWebAdmin.newInstance((ConfigWebImpl)pc.getConfig(),webAdminPassword);
+		// TODO verify password
+	}*/
+	
 	
 	public static int getSessionCount(){
 		PageContext pc = ThreadLocalPageContext.get();
@@ -19,7 +46,6 @@ public class SessionTracker {
 		ScopeContext sc = ((CFMLFactoryImpl)pc.getCFMLFactory()).getScopeContext();
 		return sc.getAllSessionScopes(appName);
 	}
-	
 	
 	/*
 	public static coldfusion.runtime.SessionScope getSession(java.lang.String,java.lang.String)
