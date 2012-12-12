a45faecdecf5b8ab40fe4b8717263896c53c800b
micstriit <michael@getrailo.com>
2012-12-12 22:21:15 +0100

    improve performance for QueryColumnCount

--- railo-java/railo-core/src/railo/runtime/functions/query/QueryColumnCount.java
+++ railo-java/railo-core/src/railo/runtime/functions/query/QueryColumnCount.java
@@ -3,12 +3,15 @@ package railo.runtime.functions.query;
 import railo.runtime.PageContext;
 import railo.runtime.ext.function.Function;
 import railo.runtime.type.Query;
+import railo.runtime.type.QueryImpl;
 
 /**
  * Implements the CFML Function querynew
  */
 public final class QueryColumnCount implements Function {
     public static double call(PageContext pc , Query qry) {
-        return qry.getColumns().length;
+    	if(qry instanceof QueryImpl)
+        	return ((QueryImpl)qry).getColumnCount();
+    	return qry.getColumnNames().length;
     }
 }
