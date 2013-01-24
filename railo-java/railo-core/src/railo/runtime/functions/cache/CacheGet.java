6cb4ddaa098c6f532bec990e112ae6ecf462c4ea
micstriit <michael@getrailo.com>
2013-01-24 21:53:30 +0100

    rewritten cacheGet for ticket https://issues.jboss.org/browse/RAILO-2270

--- railo-java/railo-core/src/railo/runtime/functions/cache/CacheGet.java
+++ railo-java/railo-core/src/railo/runtime/functions/cache/CacheGet.java
@@ -3,8 +3,10 @@ package railo.runtime.functions.cache;
 import java.io.IOException;
 
 import railo.commons.io.cache.Cache;
+import railo.commons.lang.StringUtil;
 import railo.runtime.PageContext;
 import railo.runtime.config.ConfigImpl;
+import railo.runtime.exp.FunctionException;
 import railo.runtime.exp.PageException;
 import railo.runtime.ext.function.Function;
 import railo.runtime.op.Caster;
@@ -17,19 +19,61 @@ public final class CacheGet implements Function {
 	private static final long serialVersionUID = -7164470356423036571L;
 
 	public static Object call(PageContext pc, String key) throws PageException {
-		return call(pc, key,false, null);
+		try {
+			return _call(pc, key, false, Util.getDefault(pc, ConfigImpl.CACHE_DEFAULT_OBJECT));
+		} 
+		catch (IOException e) {
+			throw Caster.toPageException(e);
+		}
+	}
+
+	public static Object call(PageContext pc, String key, Object objThrowWhenNotExist) throws PageException {
+		// default behavior, second parameter is a boolean
+		Boolean throwWhenNotExist=Caster.toBoolean(objThrowWhenNotExist,null);
+		if(throwWhenNotExist!=null) {
+			try {
+				return _call(pc, key, throwWhenNotExist.booleanValue(), Util.getDefault(pc, ConfigImpl.CACHE_DEFAULT_OBJECT));
+			} 
+			catch (IOException e) {
+				throw Caster.toPageException(e);
+			}
+		}
+		
+		// compatibility behavior, second parameter is a cacheName 
+		if(objThrowWhenNotExist instanceof String) {
+			String cacheName=(String)objThrowWhenNotExist;
+			if(!StringUtil.isEmpty(cacheName)) {
+				try {
+					Cache cache = Util.getCache(pc.getConfig(),cacheName,null);
+					
+					if(cache!=null) 
+						return _call(pc, key, false, cache);
+				} 
+				catch (IOException e) {
+					throw Caster.toPageException(e);
+				}
+			}
+		}
+		
+		// not a boolean or cacheName
+		throw new FunctionException(pc, "cacheGet", 2, "ThrowWhenNotExist", "arguments needs to be a boolean value, but also a valid cacheName is supported for compatibility reasons to other engines");
 	}
 	
-	public static Object call(PageContext pc, String key, boolean throwWhenNotExist) throws PageException {
-		return call(pc, key,throwWhenNotExist, null);
-	}
-	
-	public static Object call(PageContext pc, String key, boolean throwWhenNotExist,String cacheName) throws PageException {
+	public static Object call(PageContext pc, String key, Object objThrowWhenNotExist,String cacheName) throws PageException {
+		
+		
+		Boolean throwWhenNotExist=Caster.toBoolean(objThrowWhenNotExist,null);
+		if(throwWhenNotExist==null)throw new FunctionException(pc, "cacheGet", 2, "ThrowWhenNotExist", "arguments needs to be a boolean value");
+		
 		try {
 			Cache cache = Util.getCache(pc,cacheName,ConfigImpl.CACHE_DEFAULT_OBJECT);
-			return throwWhenNotExist?cache.getValue(Util.key(key)):cache.getValue(Util.key(key),null);
+			return _call(pc, key, throwWhenNotExist.booleanValue(), cache);
 		} catch (IOException e) {
 			throw Caster.toPageException(e);
 		}
+	}
+
+	private static Object _call(PageContext pc, String key, boolean throwWhenNotExist,Cache cache) throws IOException {
+		return throwWhenNotExist?cache.getValue(Util.key(key)):cache.getValue(Util.key(key),null);
 	}
 }
