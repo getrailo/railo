421346bd1b14458ac5d4fecb97c84a59ed137542
micstriit <michael@getrailo.com>
2013-04-11 08:36:41 +0200

    solved ticket https://issues.jboss.org/browse/RAILO-2382

--- railo-java/railo-core/src/railo/runtime/functions/string/Replace.java
+++ railo-java/railo-core/src/railo/runtime/functions/string/Replace.java
@@ -3,65 +3,50 @@
  */
 package railo.runtime.functions.string;
 
-import java.util.Map;
-
 import railo.commons.lang.StringUtil;
 import railo.runtime.PageContext;
-import railo.runtime.exp.ExpressionException;
+import railo.runtime.exp.FunctionException;
 import railo.runtime.exp.PageException;
 import railo.runtime.ext.function.Function;
+import railo.runtime.op.Caster;
+import railo.runtime.op.Decision;
 
 public final class Replace implements Function {
 	
-	
-	public static String call(PageContext pc , String str, String sub1, String sub2) throws ExpressionException {
-		
-		if ( sub1.isEmpty() )
-			throw new ExpressionException("The string length of parameter 2 of function replace must be greater than 0");
-		
-		return StringUtil.replace(str, sub1, sub2, true);
+	private static final long serialVersionUID = -313884944032266348L;
+
+	public static String call(PageContext pc , String str, String sub1, String sub2) throws FunctionException {
+		return _call(pc, str, sub1, sub2, true);
 	}
 	
-
-	public static String call(PageContext pc , String str, String sub1, String sub2, String scope) throws ExpressionException {
-		
-		if ( sub1.isEmpty() )
-			throw new ExpressionException("The string length of parameter 2 of function replace must be greater than 0");
-		
-		return StringUtil.replace(str, sub1, sub2, !scope.equalsIgnoreCase("all"));
-	}
-	
-
-	public static String call( PageContext pc, String input, Object find, String repl, String scope ) throws ExpressionException {
-		
-		if ( !( find instanceof String ) )
-			throw new ExpressionException("When passing three parameters or more, the second parameter must be a String.");
-		
-		if ( ((String)find).isEmpty() )
-			throw new ExpressionException("The string length of parameter 2 of function replace must be greater than 0");
-		
-		return StringUtil.replace( input, (String)find, repl, !scope.equalsIgnoreCase("all") );
+	public static String call(PageContext pc , String str, String sub1, String sub2, String scope) throws FunctionException {
+		return _call(pc, str, sub1, sub2, !scope.equalsIgnoreCase("all"));
 	}
 
-	
-	public static String call( PageContext pc, String input, Object find, String repl ) throws ExpressionException {
-		
-		if ( !( find instanceof String ) )
-			throw new ExpressionException("When passing three parameters or more, the second parameter must be a String.");
-		
-		if ( ((String)find).isEmpty() )
-			throw new ExpressionException("The string length of parameter 2 of function replace must be greater than 0");
-		
-		return StringUtil.replace( input, (String)find, repl, true );
+	public static String call( PageContext pc, String input, Object find, String repl, String scope ) throws PageException {
+		return _call(pc, input, find, repl, !scope.equalsIgnoreCase("all"));
 	}
-	
+
+	public static String call( PageContext pc, String input, Object find, String repl ) throws PageException {
+		return _call(pc, input, find, repl, true);
+	}
 	
 	public static String call( PageContext pc, String input, Object struct ) throws PageException {
-		
-		if ( !( struct instanceof Map ) )
-			throw new ExpressionException("When passing only two parameters, the second parameter must be a Struct.");		
-		
-		return StringUtil.replaceMap( input, (Map)struct, false );
+		if(!Decision.isStruct(struct))
+			throw new FunctionException(pc,"replace",2,"sub1","When passing only two parameters, the second parameter must be a Struct.");
+		return StringUtil.replaceMap( input, Caster.toMap(struct), false );
+	}
+	
+	private static String _call(PageContext pc , String str, String sub1, String sub2, boolean firstOnly) throws FunctionException {
+		if (StringUtil.isEmpty(sub1))
+			throw new FunctionException(pc,"replace",2,"sub1","The string length must be greater than 0");
+		return StringUtil.replace(str, sub1, sub2, firstOnly);
+	}
+	
+	private static String _call( PageContext pc, String input, Object find, String repl, boolean onlyFirst) throws PageException {
+		if(!Decision.isSimpleValue(find ) )
+			throw new FunctionException(pc,"replace",2,"sub1","When passing three parameters or more, the second parameter must be a simple value.");
+		return _call(pc, input, Caster.toString(find), repl, onlyFirst);
 	}
 
 }
--- railo-java/railo-core/src/railo/runtime/functions/string/ReplaceNoCase.java
+++ railo-java/railo-core/src/railo/runtime/functions/string/ReplaceNoCase.java
@@ -8,60 +8,46 @@ import java.util.Map;
 import railo.commons.lang.StringUtil;
 import railo.runtime.PageContext;
 import railo.runtime.exp.ExpressionException;
+import railo.runtime.exp.FunctionException;
 import railo.runtime.exp.PageException;
 import railo.runtime.ext.function.Function;
+import railo.runtime.op.Caster;
+import railo.runtime.op.Decision;
 
 public final class ReplaceNoCase implements Function {
 
-	
-	public static String call(PageContext pc , String str, String sub1, String sub2) throws ExpressionException {
-		
-		if ( sub1.isEmpty() )
-			throw new ExpressionException("The string length of parameter 2 of function replace must be greater than 0");
-		
-		return StringUtil.replace(str, sub1, sub2, true, true);
+	public static String call(PageContext pc , String str, String sub1, String sub2) throws FunctionException {
+		return _call(pc, str, sub1, sub2, true);
 	}
 	
-
-	public static String call(PageContext pc , String str, String sub1, String sub2, String scope) throws ExpressionException {
-		
-		if ( sub1.isEmpty() )
-			throw new ExpressionException("The string length of parameter 2 of function replace must be greater than 0");
-		
-		return StringUtil.replace(str, sub1, sub2, !scope.equalsIgnoreCase("all"), true);
+	public static String call(PageContext pc , String str, String sub1, String sub2, String scope) throws FunctionException {
+		return _call(pc, str, sub1, sub2, !scope.equalsIgnoreCase("all"));
 	}
-
 
 	public static String call( PageContext pc, String input, Object find, String repl, String scope ) throws ExpressionException {
-		
-		if ( !( find instanceof String ) )
-			throw new ExpressionException("When passing three parameters or more, the second parameter must be a String.");
-		
-		if ( ((String)find).isEmpty() )
-			throw new ExpressionException("The string length of parameter 2 of function replace must be greater than 0");
-		
-		return call( pc , input, (String)find, repl, scope );
+		return _call(pc, input, find, repl, !scope.equalsIgnoreCase("all") );
 	}
 
-	
 	public static String call( PageContext pc, String input, Object find, String repl ) throws ExpressionException {
-		
-		if ( !( find instanceof String ) )
-			throw new ExpressionException("When passing three parameters or more, the second parameter must be a String.");
-		
-		if ( ((String)find).isEmpty() )
-			throw new ExpressionException("The string length of parameter 2 of function replace must be greater than 0");
-		
-		return call( pc , input, (String)find, repl, "one" );
+		return _call(pc, input, find, repl, true);
 	}
 	
+	private static String _call(PageContext pc , String str, String sub1, String sub2, boolean onlyFirst) throws FunctionException {
+		if (StringUtil.isEmpty(sub1))
+			throw new FunctionException(pc,"ReplaceNoCase",2,"sub1","The string length must be greater than 0");
+		return StringUtil.replace(str, sub1, sub2, onlyFirst, true);
+	}
+	
+	private static String _call( PageContext pc, String input, Object find, String repl , boolean onlyFirst) throws ExpressionException {
+		if(!Decision.isSimpleValue(find ) )
+			throw new FunctionException(pc,"ReplaceNoCase",2,"sub1","When passing three parameters or more, the second parameter must be a String.");
+		return _call(pc, input, Caster.toString(input), repl, onlyFirst);
+	}
 	
 	public static String call( PageContext pc, String input, Object struct ) throws PageException {
-		
-		if ( !( struct instanceof Map ) )
-			throw new ExpressionException("When passing only two parameters, the second parameter must be a Struct.");		
-		
-		return StringUtil.replaceMap( input, (Map)struct, true );
+		if(!Decision.isStruct(struct))
+			throw new FunctionException(pc,"ReplaceNoCase",2,"sub1","When passing only two parameters, the second parameter must be a Struct.");
+		return StringUtil.replaceMap( input, Caster.toStruct(struct), true );
 	}
 	
 }
