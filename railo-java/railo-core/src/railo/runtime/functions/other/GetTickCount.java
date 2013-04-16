5fb5601af67300b821cdaaf2c0f14ccb0c307e79
Igal <igal@getrailo.org>
2013-04-16 10:28:13 -0700

    improved arg checking and performance

--- railo-java/railo-core/src/railo/runtime/functions/other/GetTickCount.java
+++ railo-java/railo-core/src/railo/runtime/functions/other/GetTickCount.java
@@ -3,23 +3,31 @@
  */
 package railo.runtime.functions.other;
 
+import railo.commons.lang.StringUtil;
 import railo.runtime.PageContext;
 import railo.runtime.exp.FunctionException;
 import railo.runtime.ext.function.Function;
 
 public final class GetTickCount implements Function {
+
 	public static double call(PageContext pc) {
 		return System.currentTimeMillis();
 	}
+
 	public static double call(PageContext pc,String unit) throws FunctionException {
-		unit=unit.trim();
-		if("nano".equalsIgnoreCase(unit))
-			return System.nanoTime();
-		else if("milli".equalsIgnoreCase(unit))
-			return System.currentTimeMillis();
-		else if("second".equalsIgnoreCase(unit))
-			return System.currentTimeMillis()/1000;
-		else 
-			throw new FunctionException(pc, "GetTickCount", 1, "type", "invalid value ["+unit+"], valid values are (nano,milli,second)");
+
+        if ( !StringUtil.isEmpty( unit ) ) {
+
+            char c = unit.charAt( 0 );
+
+            if ( c == 'n' || c == 'N' )
+                return System.nanoTime();
+            else if ( c == 'm' || c == 'M' )
+                return System.currentTimeMillis();
+            else if ( c == 's' || c == 'S' )
+                return System.currentTimeMillis()/1000;
+        }
+
+		throw new FunctionException(pc, "GetTickCount", 1, "unit", "invalid value ["+unit+"], valid values are (nano, milli, second)");
 	}
 }
