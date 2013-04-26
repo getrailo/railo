736bf9a2a25b185873509f6b248ede74a7aa909b
micstriit <michael@getrailo.com>
2013-04-26 11:01:57 +0200

    enable concurrent map

--- railo-java/railo-core/src/railo/commons/util/mod/MapFactory.java
+++ railo-java/railo-core/src/railo/commons/util/mod/MapFactory.java
@@ -1,8 +1,44 @@
 package railo.commons.util.mod;
 
+import railo.print;
+
 public class MapFactory {
 	public static  MapPro getConcurrentMap(){
-		return new HashMapPro();
-		//return new ConcurrentHashMapPro();
+		//return new HashMapPro();
+		return new ConcurrentHashMapPro();
+	}
+	
+	public static void main(String[] args) {
+		test(new HashMapPro());
+		test(new ConcurrentHashMapPro());
+		test(new SyncMap());
+		print.e("++++");
+		test(new HashMapPro());
+		test(new ConcurrentHashMapPro());
+		test(new SyncMap());
+		print.e("++++");
+		test(new HashMapPro());
+		test(new ConcurrentHashMapPro());
+		test(new SyncMap());
+		print.e("++++");
+		test(new HashMapPro());
+		test(new ConcurrentHashMapPro());
+		test(new SyncMap());
+	}
+
+	private static void test(MapPro map) {
+		long start =micro();
+		for(int i=0;i<10000000;i++){
+			map.put("x", "X");
+			map.get("x");
+			map.get("z");
+			map.containsKey("a");
+		}
+		print.e((micro()-start)+" -> "+map.getClass().getName());
+		
+	}
+
+	private static long micro() {
+		return System.nanoTime()/1000;
 	}
 }
