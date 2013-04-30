05b6555930b2c9c5fc21a968b7e92a5a90cd3bed
micstriit <michael@getrailo.com>
2013-04-30 12:01:14 +0200

    solved ticket https://issues.jboss.org/browse/RAILO-2390

--- railo-java/railo-core/src/railo/runtime/functions/decision/IsEmpty.java
+++ railo-java/railo-core/src/railo/runtime/functions/decision/IsEmpty.java
@@ -13,14 +13,11 @@ public class IsEmpty implements Function {
 	private static final long serialVersionUID = -2839407878650099024L;
 
 	public static boolean call(PageContext pc , Object value) throws PageException {
-
-		if ( value==null ) return true;
-
-		if ( Decision.isBoolean( value, true ) ) {
-
-			return !Caster.toBoolean( value );
-		}
-
+		if(value==null) return true;
+		if(value instanceof Boolean) return !Caster.toBoolean(value);
+		if(value instanceof Number) return Caster.toDoubleValue(value)==0D;
+		
+		
 		double len=Len.invoke(value, -1);
 		if(len==-1)throw new FunctionException(pc,"isEmpty",1,"variable","this type  ["+Caster.toTypeName(value)+"] is not supported");
 		return len==0;
