9449f4c38bde976e4afc2570f695235def1cac81
micstriit <michael@getrailo.com>
2013-05-01 00:16:18 +0200

    removed test code

--- railo-java/railo-core/src/railo/commons/util/mod/MapFactory.java
+++ railo-java/railo-core/src/railo/commons/util/mod/MapFactory.java
@@ -7,38 +7,4 @@ public class MapFactory {
 		//return new HashMapPro();
 		return new ConcurrentHashMapPro();
 	}
-	
-	public static void main(String[] args) {
-		test(new HashMapPro());
-		test(new ConcurrentHashMapPro());
-		test(new SyncMap());
-		print.e("++++");
-		test(new HashMapPro());
-		test(new ConcurrentHashMapPro());
-		test(new SyncMap());
-		print.e("++++");
-		test(new HashMapPro());
-		test(new ConcurrentHashMapPro());
-		test(new SyncMap());
-		print.e("++++");
-		test(new HashMapPro());
-		test(new ConcurrentHashMapPro());
-		test(new SyncMap());
-	}
-
-	private static void test(MapPro map) {
-		long start =micro();
-		for(int i=0;i<10000000;i++){
-			map.put("x", "X");
-			map.get("x");
-			map.get("z");
-			map.containsKey("a");
-		}
-		print.e((micro()-start)+" -> "+map.getClass().getName());
-		
-	}
-
-	private static long micro() {
-		return System.nanoTime()/1000;
-	}
 }
