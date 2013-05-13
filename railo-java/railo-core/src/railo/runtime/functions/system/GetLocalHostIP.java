b6c11308e41cbf1f5c7bfcee8caaeeb467f97822
Igal <igal@getrailo.org>
2013-05-12 18:37:11 -0700

    added optional args to BIF GetLocalHostIP()

--- railo-java/railo-core/src/railo/runtime/functions/system/GetLocalHostIP.java
+++ railo-java/railo-core/src/railo/runtime/functions/system/GetLocalHostIP.java
@@ -7,15 +7,38 @@ import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import railo.commons.net.IPUtil;
 import railo.runtime.PageContext;
 import railo.runtime.ext.function.Function;
 
 public final class GetLocalHostIP implements Function {
-	public static String call(PageContext pc)  {
-		try {
+
+	public static Object call(PageContext pc)  {
+		return callLegacy();
+	}
+
+
+    public static Object call(PageContext pc, boolean all, boolean refresh) {
+
+        if ( all )
+            return IPUtil.getLocalIPs( refresh );
+
+        return callLegacy();
+    }
+
+
+    public static Object call(PageContext pc, boolean all) {
+
+        return call( pc, all, false );
+    }
+
+
+    static String callLegacy() {
+
+        try {
             if(InetAddress.getLocalHost() instanceof Inet6Address) return "::1";
         }
         catch(UnknownHostException e) {}
         return "127.0.0.1";
-	}
+    }
 }
