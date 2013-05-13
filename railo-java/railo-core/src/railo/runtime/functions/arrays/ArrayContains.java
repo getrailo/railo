b4b78e9d55bb675ff9c01dff384112f3eaef4bbd
micstriit <michael@getrailo.com>
2013-05-13 11:06:03 +0200

    a non boolean 3th parameter should not be ignored, it should throw a cast exception

--- railo-java/railo-core/src/railo/runtime/functions/arrays/ArrayContains.java
+++ railo-java/railo-core/src/railo/runtime/functions/arrays/ArrayContains.java
@@ -17,34 +17,27 @@ public final class ArrayContains extends BIF {
 	private static final long serialVersionUID = -5400552848978801342L;
 
 	public static double call(PageContext pc, Array array, Object value, boolean substringMatch) throws PageException {
-
-        if ( substringMatch ) {
-
-            if ( !Decision.isSimpleValue( value ) )
+        if (substringMatch) {
+            if (!Decision.isSimpleValue(value))
                 throw new FunctionException( pc, "ArrayContains", 3, "substringMatch", "substringMatch can not be true when the value that is searched for is a complex object" );
-
             return callLegacy( pc, array, value );
         }
-
         return ArrayFind.call( pc, array, value );
 	}
 
     public static double call(PageContext pc , Array array, Object value) throws PageException {
-
         return call( pc, array, value, false );
     }
 
 	@Override
 	public Object invoke(PageContext pc, Object[] args) throws PageException {
-
-        if ( args.length > 2 && Decision.isBoolean( args[2] ) )
+        if ( args.length > 2)
     		return call( pc, Caster.toArray(args[0]),args[1], Caster.toBoolean( args[2] ) );
-
         return call(pc,Caster.toArray(args[0]),args[1]);
 	}
 
 
-    /** legacy implementation */
+    /* legacy implementation */
     static double callLegacy(PageContext pc, Array array, Object value) throws PageException {
         String str=Caster.toString(value,null);
         if(str!=null)
