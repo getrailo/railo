52ac8a5ccaa38a75f70f6af9bb7600f0ca27b3a1
Igal <igal@getrailo.org>
2013-07-04 11:24:45 -0700

    added IV/Salt+Iterations to encrypt/decrypt functions

--- railo-java/railo-core/src/railo/runtime/functions/other/Decrypt.java
+++ railo-java/railo-core/src/railo/runtime/functions/other/Decrypt.java
@@ -11,40 +11,70 @@ import railo.runtime.exp.PageException;
 import railo.runtime.ext.function.Function;
 import railo.runtime.op.Caster;
 
-//FUTURE 2 attr fehlen noch
 
 public final class Decrypt implements Function {
-    
-    
-    public synchronized static String call(PageContext pc , String string, String key) throws PageException {
-    	return call(pc,string,key,"cfmx_compat","uu");
-	}
 
-    public synchronized static String call(PageContext pc , String string, String key, String algorithm) throws PageException {
-    	return call(pc,string,key,algorithm,"uu");
-	}
-    public synchronized static String call(PageContext pc , String string, String key, String algorithm, String encoding) throws PageException {
-    	return invoke(string, key, algorithm, encoding);
-	}
-    
 
-    protected synchronized static String invoke(String string, String key, String algorithm, String encoding) throws PageException {
-    	try {
-			return new String(invoke(Coder.decode(encoding, string), key, algorithm),"UTF-8");
-		} 
-    	catch (Exception e) {
-			throw Caster.toPageException(e);
-		}
-	}
-    protected synchronized static byte[] invoke(byte[] input, String key, String algorithm) throws PageException {
-    	try {
-    		if("cfmx_compat".equalsIgnoreCase(algorithm)){
-	    		return new CFMXCompat().transformString(key, input);
-	    	}
-	    	return Cryptor.decrypt(algorithm, key, input);	
-		} 
-    	catch (Exception e) {
-			throw Caster.toPageException(e);
-		}
-	}
+    public synchronized static String call( PageContext pc, String input, String key ) throws PageException {
+
+        return invoke(input, key, CFMXCompat.ALGORITHM_NAME, Cryptor.DEFAULT_ENCODING, null, 0);
+    }
+
+
+    public synchronized static String call( PageContext pc, String input, String key, String algorithm ) throws PageException {
+
+        return invoke(input, key, algorithm, Cryptor.DEFAULT_ENCODING, null, 0);
+    }
+
+
+    public synchronized static String call( PageContext pc, String input, String key, String algorithm, String encoding ) throws PageException {
+
+        return invoke( input, key, algorithm, encoding, null, 0 );
+    }
+
+
+    public synchronized static String call( PageContext pc, String input, String key, String algorithm, String encoding, Object ivOrSalt ) throws PageException {
+
+        return invoke(input, key, algorithm, encoding, ivOrSalt, 0);
+    }
+
+
+    /**
+     * call with all optional args
+     */
+    public synchronized static String call( PageContext pc, String input, String key, String algorithm, String encoding, Object ivOrSalt, double iterations ) throws PageException {
+
+        return invoke( input, key, algorithm, encoding, ivOrSalt, Caster.toInteger( iterations ) );
+    }
+
+
+    public synchronized static String invoke( String input, String key, String algorithm, String encoding, Object ivOrSalt, int iterations ) throws PageException {
+
+        try {
+
+            if ( CFMXCompat.isCfmxCompat( algorithm ) )
+                return Coder.encode( encoding, new CFMXCompat().transformString( key, input.getBytes() ) );
+
+            byte[] baIVS = null;
+            if ( ivOrSalt instanceof String )
+                baIVS = ((String)ivOrSalt).getBytes( Cryptor.DEFAULT_CHARSET );
+            else if ( ivOrSalt != null )
+                baIVS = Caster.toBinary( ivOrSalt );
+
+            return Cryptor.decrypt( input, key, algorithm, baIVS, iterations, encoding, Cryptor.DEFAULT_CHARSET  );
+        }
+        catch ( Throwable t ) {
+
+            throw Caster.toPageException( t );
+        }
+    }
+
+
+    public synchronized static byte[] invoke( byte[] input, String key, String algorithm, byte[] ivOrSalt, int iterations ) throws PageException {
+
+        if ( CFMXCompat.isCfmxCompat( algorithm ) )
+            return new CFMXCompat().transformString( key, input );
+
+        return Cryptor.decrypt( input, key, algorithm, ivOrSalt, iterations );
+    }
 }
