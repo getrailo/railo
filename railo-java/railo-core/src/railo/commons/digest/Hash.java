16cfa3d2c9ffe2a42c3b502b50b654c8becba758
micstriit <michael@getrailo.com>
2013-07-19 19:13:53 +0200

    improved hash gnerating for ticket https://issues.jboss.org/browse/RAILO-2529

--- railo-java/railo-core/src/railo/commons/digest/Hash.java
+++ railo-java/railo-core/src/railo/commons/digest/Hash.java
@@ -10,6 +10,7 @@ import railo.commons.io.CharsetUtil;
 public class Hash {
 	
 	public static final char[] ENCODING_HEX = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+	public static final char[] ENCODING_HEXUC = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 	//public static final char[] ENCODING_ASCII = "0123456789abcdefghijklmnopqrstuvwxyz".toCharArray();
 	private static final byte[] DEL=new byte[]{58};
 	
@@ -20,28 +21,72 @@ public class Hash {
 	public static final String ALGORITHM_SHA_512="SHA-512";
 	public static final String ALGORITHM_SHA="SHA";
 
-	public static void main(String[] args) throws Throwable {
-		hash("key","nonce",ALGORITHM_SHA_256,ENCODING_HEX);
+	// MD5
+	public static String md5(byte[] data) throws NoSuchAlgorithmException {
+		return hash(data, ALGORITHM_MD5,ENCODING_HEX);
 	}
 	
 	public static String md5(String str) throws NoSuchAlgorithmException {
-		return hash(str, ALGORITHM_MD5,ENCODING_HEX);
+		return hash(str, ALGORITHM_MD5,ENCODING_HEX,CharsetUtil.UTF8);
+	}
+	
+	public static String md5(String str, Charset charset) throws NoSuchAlgorithmException {
+		return hash(str, ALGORITHM_MD5,ENCODING_HEX,charset);
+	}
+	
+	// SHA
+	public static String sha(byte[] data) throws NoSuchAlgorithmException {
+		return hash(data, ALGORITHM_SHA,ENCODING_HEX);
 	}
 	
 	public static String sha(String str) throws NoSuchAlgorithmException {
-		return hash(str, ALGORITHM_SHA,ENCODING_HEX);
+		return hash(str, ALGORITHM_SHA,ENCODING_HEX,CharsetUtil.UTF8);
+	}
+	
+	public static String sha(String str,Charset charset) throws NoSuchAlgorithmException {
+		return hash(str, ALGORITHM_SHA,ENCODING_HEX,charset);
+	}
+
+	
+	// SHA256
+	public static String sha256(byte[] data) throws NoSuchAlgorithmException {
+		return hash(data, ALGORITHM_SHA_256,ENCODING_HEX);
 	}
 	
 	public static String sha256(String str) throws NoSuchAlgorithmException {
-		return hash(str, ALGORITHM_SHA_256,ENCODING_HEX);
+		return hash(str, ALGORITHM_SHA_256,ENCODING_HEX,CharsetUtil.UTF8);
+	}
+	
+	public static String sha256(String str,Charset charset) throws NoSuchAlgorithmException {
+		return hash(str, ALGORITHM_SHA_256,ENCODING_HEX,charset);
+	}
+
+	
+	// SHA384
+	public static String sha384(byte[] data) throws NoSuchAlgorithmException {
+		return hash(data, ALGORITHM_SHA_384,ENCODING_HEX);
 	}
 	
 	public static String sha384(String str) throws NoSuchAlgorithmException {
-		return hash(str, ALGORITHM_SHA_384,ENCODING_HEX);
+		return hash(str, ALGORITHM_SHA_384,ENCODING_HEX,CharsetUtil.UTF8);
+	}
+	
+	public static String sha384(String str,Charset charset) throws NoSuchAlgorithmException {
+		return hash(str, ALGORITHM_SHA_384,ENCODING_HEX,charset);
+	}
+
+	
+	// SHA384
+	public static String sha512(byte[] data) throws NoSuchAlgorithmException {
+		return hash(data, ALGORITHM_SHA_512,ENCODING_HEX);
 	}
 	
 	public static String sha512(String str) throws NoSuchAlgorithmException {
-		return hash(str, ALGORITHM_SHA_512,ENCODING_HEX);
+		return hash(str, ALGORITHM_SHA_512,ENCODING_HEX,CharsetUtil.UTF8);
+	}
+	
+	public static String sha512(String str,Charset charset) throws NoSuchAlgorithmException {
+		return hash(str, ALGORITHM_SHA_512,ENCODING_HEX,charset);
 	}
 	
 	
@@ -53,8 +98,10 @@ public class Hash {
 	    md.update(toBytes(nonce, CharsetUtil.UTF8));
 	    return new String( enc(md.digest(),encoding)); // no charset needed because all characters are below us-ascii (hex)
 	}
-	
 
+	public static String hash(String input, String algorithm, int numIterations) throws NoSuchAlgorithmException {
+		return hash(input, algorithm, numIterations, ENCODING_HEXUC);
+	}
 	
 	public static String hash(String str, String algorithm, int numIterations,char[] encoding) throws NoSuchAlgorithmException {
 		try {
@@ -71,15 +118,21 @@ public class Hash {
 		
 		// if not possible to clone the MessageDigest create always a new instance
 		for(int i=0;i<numIterations;i++){
-			str=hash(str, algorithm,encoding);
+			str=hash(str, algorithm,encoding,CharsetUtil.UTF8);
 		}
 		return str;
 	}
 	
-	public static String hash(String str, String algorithm,char[] encoding) throws NoSuchAlgorithmException {
+
+	
+	public static String hash(String str, String algorithm,char[] encoding, Charset charset) throws NoSuchAlgorithmException {
+		return hash(toBytes(str, charset), algorithm,encoding); 
+	}
+	
+	public static String hash(byte[] data, String algorithm,char[] encoding) throws NoSuchAlgorithmException {
 		MessageDigest md=MessageDigest.getInstance(algorithm);
 	    md.reset();
-	    md.update(toBytes(str, CharsetUtil.UTF8));
+	    md.update(data);
 	    return new String( enc(md.digest(),encoding)); // no charset needed because all characters are below us-ascii (hex)
 	}
 	
@@ -88,6 +141,9 @@ public class Hash {
 		return str.getBytes(charset);
 	}
 	
+	public static String toHexString(byte[] data, boolean upperCase) {
+		return new String(enc(data, upperCase?ENCODING_HEXUC:ENCODING_HEX));
+	}
 	private static char[] enc(byte[] data,char[] enc) {
 		
 		int len = data.length;
