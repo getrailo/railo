a318d35871f857bf9fa06444e5a4ec2735a31c39
micstriit <michael@getrailo.com>
2013-08-05 11:42:18 +0200

    solved ticket https://issues.jboss.org/browse/RAILO-2559

--- railo-java/railo-core/src/railo/runtime/functions/string/Hash.java
+++ railo-java/railo-core/src/railo/runtime/functions/string/Hash.java
@@ -5,12 +5,17 @@ package railo.runtime.functions.string;
 
 import java.security.MessageDigest;
 
+import railo.commons.digest.HashUtil;
 import railo.commons.lang.StringUtil;
+import railo.commons.lang.SystemOut;
 import railo.runtime.PageContext;
 import railo.runtime.config.Config;
+import railo.runtime.engine.ThreadLocalPageContext;
+import railo.runtime.exp.FunctionException;
 import railo.runtime.exp.PageException;
 import railo.runtime.ext.function.Function;
 import railo.runtime.op.Caster;
+import railo.runtime.tag.util.DeprecatedUtil;
 
 public final class Hash implements Function {
 	
@@ -54,6 +59,13 @@ public final class Hash implements Function {
     	if(StringUtil.isEmpty(algorithm))algorithm="md5";
 		else algorithm=algorithm.trim().toLowerCase();
     	if("cfmx_compat".equals(algorithm)) algorithm="md5";
+    	else if("quick".equals(algorithm)) {
+    		if(numIterations>1) 
+    			SystemOut.printDate("for algorithm [quick], argument numIterations makes no sense, because this algorithm has no security in mind");
+    		return HashUtil.create64BitHashAsString(Caster.toString(input), 16);
+    	}
+    	
+    	
 		
     	if(StringUtil.isEmpty(encoding))encoding=config.getWebCharset();
 		byte[] data = null;
