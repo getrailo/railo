9117b4cf7c6544c8c1c0fcd2d9643e6cf7ac6dc8
Igal <igal@getrailo.org>
2013-09-15 10:04:31 -0700

    moved snippet store to a new class for code reuse and maint

--- railo-java/railo-core/src/railo/runtime/engine/DebugExecutionLog.java
+++ railo-java/railo-core/src/railo/runtime/engine/DebugExecutionLog.java
@@ -1,22 +1,17 @@
 package railo.runtime.engine;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
-import railo.commons.io.res.Resource;
 import railo.commons.io.res.util.ResourceSnippet;
+import railo.commons.io.res.util.ResourceSnippetsMap;
 import railo.runtime.PageContext;
-import railo.runtime.PageSource;
 import railo.runtime.debug.DebugEntry;
 import railo.runtime.debug.DebugEntryTemplatePartImpl;
 
 public class DebugExecutionLog extends ExecutionLogSupport {
 	
 	private PageContext pc;
-
-    private Map<String, String> sources           = Collections.synchronizedMap(new HashMap());
-    private Map<String, ResourceSnippet> snippets = Collections.synchronizedMap(new HashMap());
+	private ResourceSnippetsMap snippetsMap = new ResourceSnippetsMap( 1024, 128 );
 
 	protected void _init(PageContext pc, Map<String, String> arguments) {
 		this.pc=pc;
@@ -24,46 +19,29 @@ public class DebugExecutionLog extends ExecutionLogSupport {
 	
 	@Override
 	protected void _log(int startPos, int endPos, long startTime, long endTime) {
+
 		if(!pc.getConfig().debug()) return;
 
 		long diff=endTime-startTime;
 		if(unit==UNIT_MICRO)diff/=1000;
 		else if(unit==UNIT_MILLI)diff/=1000000;
-        PageSource ps = pc.getCurrentPageSource();
-        Resource res = ps.getResource();
 
-		String keyRes = res.getAbsolutePath() + "@" + res.lastModified();
-        String keySnp = ps.getDisplayPath()   + "@" + ps.getLastAccessTime() + ":" + startPos + "-" + endPos;
-
-        DebugEntry de = pc.getDebugger().getEntry(pc, ps, startPos, endPos);
+        DebugEntry de = pc.getDebugger().getEntry(pc, pc.getCurrentPageSource(), startPos, endPos);
         de.updateExeTime((int) diff);
 
-        ResourceSnippet snippet = snippets.get( keySnp );
+		if ( de instanceof DebugEntryTemplatePartImpl ) {
 
-        if ( snippet == null ) {
+			ResourceSnippet snippet = snippetsMap.getSnippet( pc.getCurrentPageSource(), startPos, endPos, pc.getConfig().getResourceCharset() );
 
-            String src = sources.get( keyRes );
-
-            if ( src == null ) {
-                src = ResourceSnippet.getContents( res, pc.getConfig().getResourceCharset() );
-                sources.put( keyRes, src );
-            }
-
-            snippet = ResourceSnippet.createResourceSnippet( src, startPos, endPos );
-            snippets.put( keySnp, snippet );
-
-            if ( de instanceof DebugEntryTemplatePartImpl ) {
-                ( (DebugEntryTemplatePartImpl)de).setStartLine( snippet.getStartLine() );
-                ( (DebugEntryTemplatePartImpl)de).setEndLine( snippet.getEndLine() );
-                ( (DebugEntryTemplatePartImpl)de).setSnippet( snippet.getContent() );
-            }
-        }
+			( (DebugEntryTemplatePartImpl)de).setStartLine( snippet.getStartLine() );
+			( (DebugEntryTemplatePartImpl)de).setEndLine( snippet.getEndLine() );
+			( (DebugEntryTemplatePartImpl)de).setSnippet( snippet.getContent() );
+		}
 	}
 
 
 	@Override
 	protected void _release() {
-
 	}
 
 }
