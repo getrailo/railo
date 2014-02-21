4be07a0af3a3880d394377bfb12181ee84874e46
micstriit <michael@getrailo.com>
2014-02-21 12:22:31 +0100

    solved ticket https://issues.jboss.org/browse/RAILO-2202

--- railo-java/railo-core/src/railo/runtime/functions/system/CFFunction.java
+++ railo-java/railo-core/src/railo/runtime/functions/system/CFFunction.java
@@ -17,7 +17,9 @@ import railo.runtime.type.KeyImpl;
 import railo.runtime.type.Struct;
 import railo.runtime.type.StructImpl;
 import railo.runtime.type.UDF;
+import railo.runtime.type.UDFImpl;
 import railo.runtime.type.UDFPlus;
+import railo.runtime.type.scope.UndefinedImpl;
 import railo.runtime.type.scope.Variables;
 import railo.runtime.type.scope.VariablesImpl;
 import railo.runtime.type.util.ArrayUtil;
@@ -41,40 +43,48 @@ public class CFFunction {
 		
 		UDF udf=loadUDF(pc, filename, name, isweb);
 		Struct meta = udf.getMetaData(pc);
+		boolean callerScopes=(meta==null)?false:Caster.toBooleanValue(meta.get("callerScopes",Boolean.FALSE),false);
 		boolean caller=meta==null?false:Caster.toBooleanValue(meta.get(KeyConstants._caller,Boolean.FALSE),false);
 		
-		Struct namedArguments=null;
+		Struct namedArguments=null,cs=null;
+		if(callerScopes) {
+			
+			cs=new StructImpl();
+			if(pc.undefinedScope().getCheckArguments()) {
+				cs.set(KeyConstants._local, pc.localScope().duplicate(false));
+				cs.set(KeyConstants._arguments, pc.argumentsScope().duplicate(false));
+			}
+		}
+		
 		Object[] arguments=null;
 		if(objArr.length<=3)arguments=ArrayUtil.OBJECT_EMPTY;
 		else if(objArr[3] instanceof FunctionValue){
 			FunctionValue fv;
 			namedArguments=new StructImpl();
-			if(caller)namedArguments.setEL(KeyConstants._caller, Duplicator.duplicate(pc.undefinedScope(),false));
+			if(callerScopes)	namedArguments.setEL(KeyConstants._caller, cs);
+			else if(caller)		namedArguments.setEL(KeyConstants._caller, Duplicator.duplicate(pc.undefinedScope(),false));
 			for(int i=3;i<objArr.length;i++){
 				fv=toFunctionValue(name,objArr[i]);
 				namedArguments.set(fv.getName(), fv.getValue());
 			}
 		}
 		else {
-			int offset=(caller?2:3);
+			int offset=(caller||callerScopes?2:3);
 			arguments=new Object[objArr.length-offset];
-			if(caller)arguments[0]=Duplicator.duplicate(pc.undefinedScope(),false);
+			if(callerScopes) arguments[0]=cs;
+			else if(caller)arguments[0]=Duplicator.duplicate(pc.undefinedScope(),false);
 			for(int i=3;i<objArr.length;i++){
 				arguments[i-offset]=toObject(name,objArr[i]);
 			}
 		}
 		
-		
-		// load UDF
-		
-		
 		// execute UDF
 		if(namedArguments==null){
-			return ((UDFPlus)udf).call(pc,name, arguments, false);
+			return ((UDFImpl)udf).call(pc,name, arguments, false);
 		}
 		
 		
-		return ((UDFPlus)udf).callWithNamedValues(pc,name, namedArguments, false);
+		return ((UDFImpl)udf).callWithNamedValues(pc,name, namedArguments, false);
 	}
 
 	public static synchronized UDF loadUDF(PageContext pc, String filename,Collection.Key name,boolean isweb) throws PageException {
