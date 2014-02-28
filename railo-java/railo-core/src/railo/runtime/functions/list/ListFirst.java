09ad0e65c141092c70127d7063611ee0717f2745
Igal <igal@getrailo.org>
2014-02-28 09:02:35 -0800

    added check to ensure count/offset is greater than 0

--- railo-java/railo-core/src/railo/runtime/functions/list/ListFirst.java
+++ railo-java/railo-core/src/railo/runtime/functions/list/ListFirst.java
@@ -4,6 +4,7 @@
 package railo.runtime.functions.list;
 
 import railo.runtime.PageContext;
+import railo.runtime.exp.FunctionException;
 import railo.runtime.ext.function.Function;
 import railo.runtime.type.util.ListUtil;
 
@@ -23,7 +24,10 @@ public final class ListFirst implements Function {
 		return ListUtil.first(list, delimiter, !includeEmptyFields, 1);
 	}
 
-	public static String call(PageContext pc, String list, String delimiter, boolean includeEmptyFields, double count) {
+	public static String call(PageContext pc, String list, String delimiter, boolean includeEmptyFields, double count) throws FunctionException {
+
+		if (count < 1)
+			throw new FunctionException(pc, "ListFirst", 4, "count", "Argument count must be a positive value greater than 0");
 
 		return ListUtil.first(list, delimiter, !includeEmptyFields, (int)count);
 	}
--- railo-java/railo-core/src/railo/runtime/functions/list/ListRest.java
+++ railo-java/railo-core/src/railo/runtime/functions/list/ListRest.java
@@ -4,6 +4,7 @@
 package railo.runtime.functions.list;
 
 import railo.runtime.PageContext;
+import railo.runtime.exp.FunctionException;
 import railo.runtime.ext.function.Function;
 import railo.runtime.type.util.ListUtil;
 
@@ -21,7 +22,10 @@ public final class ListRest implements Function {
 		return ListUtil.rest(list, delimiter, !includeEmptyFields, 1);
 	}
 
-	public static String call(PageContext pc, String list, String delimiter, boolean includeEmptyFields, double offset) {
+	public static String call(PageContext pc, String list, String delimiter, boolean includeEmptyFields, double offset) throws FunctionException {
+
+		if (offset < 1)
+			throw new FunctionException(pc, "ListFirst", 4, "offset", "Argument offset must be a positive value greater than 0");
 
 		return ListUtil.rest(list, delimiter, !includeEmptyFields, (int)offset);
 	}
