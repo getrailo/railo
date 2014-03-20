d44ce33ee4cc6761a5337033d725e5b8daea753d
Colby Litnak <colby_litnak@hotmail.com>
2014-03-20 10:32:18 -0600

    vertical axis's too small for labels.

--- railo-java/railo-core/src/railo/runtime/tag/Chart.java
+++ railo-java/railo-core/src/railo/runtime/tag/Chart.java
@@ -1164,6 +1164,7 @@ public final class Chart extends BodyTagImpl implements Serializable {
 			CategoryAxis da = cp.getDomainAxis();
 			if(!showXLabel)da.setTickLabelsVisible(false);
 			da.setCategoryLabelPositions(labelPosition);
+			da.setMaximumCategoryLabelWidthRatio(100);
 			//da.setVisible(false);
 		}
 		if(plot instanceof XYPlot) {
