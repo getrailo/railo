b86e99cd5909cbf430d6f8b854cfe3c91e9d9062
micstriit <michael@getrailo.com>
2014-03-20 21:01:45 +0100

    solved ticket https://issues.jboss.org/browse/RAILO-2974

---
+++ railo-java/railo-core/src/railo/runtime/functions/struct/StructEvery.java
@@ -0,0 +1,46 @@
+package railo.runtime.functions.struct;
+
+import railo.runtime.PageContext;
+import railo.runtime.exp.FunctionException;
+import railo.runtime.exp.PageException;
+import railo.runtime.functions.BIF;
+import railo.runtime.functions.closure.Every;
+import railo.runtime.op.Caster;
+import railo.runtime.type.Array;
+import railo.runtime.type.Struct;
+import railo.runtime.type.UDF;
+
+public class StructEvery extends BIF {
+
+	private static final long serialVersionUID = 7565871828647900243L;
+
+	public static boolean call(PageContext pc , Struct sct, UDF udf) throws PageException {
+		return _call(pc, sct, udf, false, 20);
+	}
+	public static boolean call(PageContext pc , Struct sct, UDF udf, boolean parallel) throws PageException {
+		return _call(pc, sct, udf, parallel, 20);
+	}
+
+	public static boolean call(PageContext pc , Struct sct, UDF udf, boolean parallel, double maxThreads) throws PageException {
+		return _call(pc, sct, udf, parallel, (int)maxThreads);
+	}
+	private static boolean _call(PageContext pc , Struct sct, UDF udf, boolean parallel, int maxThreads) throws PageException {
+		return Every._call(pc, sct, udf, parallel, maxThreads);
+	}
+
+	@Override
+	public Object invoke(PageContext pc, Object[] args) throws PageException {
+		
+		if(args.length==2)
+			return call(pc, Caster.toStruct(args[0]), Caster.toFunction(args[1]));
+		if(args.length==3)
+			return call(pc, Caster.toStruct(args[0]), Caster.toFunction(args[1]), Caster.toBooleanValue(args[2]));
+		if(args.length==4)
+			return call(pc, Caster.toStruct(args[0]), Caster.toFunction(args[1]), Caster.toBooleanValue(args[2]), Caster.toDoubleValue(args[3]));
+		
+		throw new FunctionException(pc, "StructEvery", 2, 4, args.length);
+		
+		
+	}
+
+}
