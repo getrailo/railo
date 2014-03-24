5ea848d62f7afee247a02466017f89d24d7dea45
micstriit <michael@getrailo.com>
2014-03-24 10:38:58 +0100

    solved ticket https://issues.jboss.org/browse/RAILO-2987

---
+++ railo-java/railo-core/src/railo/runtime/functions/query/QueryEvery.java
@@ -0,0 +1,45 @@
+package railo.runtime.functions.query;
+
+import railo.runtime.PageContext;
+import railo.runtime.exp.FunctionException;
+import railo.runtime.exp.PageException;
+import railo.runtime.functions.BIF;
+import railo.runtime.functions.closure.Every;
+import railo.runtime.op.Caster;
+import railo.runtime.type.Query;
+import railo.runtime.type.UDF;
+
+public class QueryEvery extends BIF {
+
+	private static final long serialVersionUID = -9206289776586881074L;
+
+	public static boolean call(PageContext pc , Query qry, UDF udf) throws PageException {
+		return _call(pc, qry, udf, false, 20);
+	}
+	public static boolean call(PageContext pc , Query qry, UDF udf, boolean parallel) throws PageException {
+		return _call(pc, qry, udf, parallel, 20);
+	}
+
+	public static boolean call(PageContext pc , Query qry, UDF udf, boolean parallel, double maxThreads) throws PageException {
+		return _call(pc, qry, udf, parallel, (int)maxThreads);
+	}
+	private static boolean _call(PageContext pc , Query qry, UDF udf, boolean parallel, int maxThreads) throws PageException {
+		return Every._call(pc, qry, udf, parallel, maxThreads);
+	}
+
+	@Override
+	public Object invoke(PageContext pc, Object[] args) throws PageException {
+		
+		if(args.length==2)
+			return call(pc, Caster.toQuery(args[0]), Caster.toFunction(args[1]));
+		if(args.length==3)
+			return call(pc, Caster.toQuery(args[0]), Caster.toFunction(args[1]), Caster.toBooleanValue(args[2]));
+		if(args.length==4)
+			return call(pc, Caster.toQuery(args[0]), Caster.toFunction(args[1]), Caster.toBooleanValue(args[2]), Caster.toDoubleValue(args[3]));
+		
+		throw new FunctionException(pc, "QueryEvery", 2, 4, args.length);
+		
+		
+	}
+
+}
