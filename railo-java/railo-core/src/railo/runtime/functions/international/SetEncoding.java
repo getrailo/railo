82174558325d5bf52584911456cd5a330f8e2320
micstriit <michael@getrailo.com>
2014-03-26 09:20:56 +0100

    typo

--- railo-java/railo-core/src/railo/runtime/functions/international/SetEncoding.java
+++ railo-java/railo-core/src/railo/runtime/functions/international/SetEncoding.java
@@ -9,7 +9,7 @@ import railo.runtime.ext.function.Function;
 import railo.runtime.op.Caster;
 
 /**
- * Implements the CFML Function setlocale
+ * Implements the CFML Function SetEncoding
  */
 public final class SetEncoding implements Function {
       
