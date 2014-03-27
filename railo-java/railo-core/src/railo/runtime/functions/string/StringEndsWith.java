740e9fbf8841479f510c652a880d640ae12df4b2
Igal <igal@getrailo.org>
2014-03-27 13:26:49 -0700

    added support for calling as BIF and test case

--- railo-java/railo-core/src/railo/runtime/functions/string/StringEndsWith.java
+++ railo-java/railo-core/src/railo/runtime/functions/string/StringEndsWith.java
@@ -11,18 +11,25 @@ import railo.runtime.op.Caster;
  */
 public class StringEndsWith extends BIF {
 
+	public static boolean call(PageContext pc, String input, String subs, boolean ignoreCase) {
+
+		if (ignoreCase)
+			return input.regionMatches(true, input.length() - subs.length(), subs, 0, subs.length());
+
+		return input.endsWith(subs);
+	}
+
+	public static boolean call(PageContext pc, String input, String subs) {
+
+		return call(pc, input, subs, false);
+	}
+
 	@Override
 	public Object invoke(PageContext pc, Object[] args) throws PageException {
 
 		if (args.length < 2 || args.length > 3)
-			throw new FunctionException(pc, "startsWith", 2, 3, args.length);
+			throw new FunctionException(pc, "endsWith", 2, 3, args.length);
 
-		String input = Caster.toString(args[0]);
-		String subs  = Caster.toString(args[1]);
-
-		if (args.length == 2 || !Caster.toBoolean(args[2]))
-			return input.endsWith(subs);
-
-		return input.regionMatches(true, input.length() - subs.length(), subs, 0, subs.length());
+		return call(pc, Caster.toString(args[0]), Caster.toString(args[1]), args.length == 3 ? Caster.toBoolean(args[2]) : false);
 	}
 }
--- railo-java/railo-core/src/railo/runtime/functions/string/StringStartsWith.java
+++ railo-java/railo-core/src/railo/runtime/functions/string/StringStartsWith.java
@@ -11,18 +11,25 @@ import railo.runtime.op.Caster;
  */
 public class StringStartsWith extends BIF {
 
+	public static boolean call(PageContext pc, String input, String subs, boolean ignoreCase) {
+
+		if (ignoreCase)
+			return input.regionMatches(true, 0, subs, 0, subs.length());
+
+		return input.startsWith(subs);
+	}
+
+	public static boolean call(PageContext pc, String input, String subs) {
+
+		return call(pc, input, subs, false);
+	}
+
 	@Override
 	public Object invoke(PageContext pc, Object[] args) throws PageException {
 
 		if (args.length < 2 || args.length > 3)
 			throw new FunctionException(pc, "startsWith", 2, 3, args.length);
 
-		String input = Caster.toString(args[0]);
-		String subs  = Caster.toString(args[1]);
-
-		if (args.length == 2 || !Caster.toBoolean(args[2]))
-			return input.startsWith(subs);
-
-		return input.regionMatches(true, 0, subs, 0, subs.length());
+		return call(pc, Caster.toString(args[0]), Caster.toString(args[1]), args.length == 3 ? Caster.toBoolean(args[2]) : false);
 	}
 }
