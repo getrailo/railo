1dc35238cbf4c3174e8d764c805813fc48a2a2b8
micstriit <michael@getrailo.com>
2014-04-18 09:30:17 +0200

    solved ticket https://issues.jboss.org/browse/RAILO-3037

--- railo-java/railo-core/src/railo/runtime/functions/dateTime/DateConvert.java
+++ railo-java/railo-core/src/railo/runtime/functions/dateTime/DateConvert.java
@@ -4,14 +4,17 @@
 package railo.runtime.functions.dateTime;
 
 import railo.runtime.PageContext;
-import railo.runtime.exp.ExpressionException;
+import railo.runtime.exp.ApplicationException;
 import railo.runtime.ext.function.Function;
 import railo.runtime.type.dt.DateTime;
-import railo.runtime.type.dt.DateTimeImpl;
 
 public final class DateConvert implements Function {
-	public static DateTime call(PageContext pc , String conversionType, DateTime date) throws ExpressionException {
-		int offset = pc.getTimeZone().getOffset(date.getTime());
+	public static DateTime call(PageContext pc , String conversionType, DateTime date) throws ApplicationException {
+		
+		throw new ApplicationException("This function is no longer supported, because it gives you the wrong impression that the timezone is part of the date object, what is wrong!" +
+				"When you wanna convert a Date to String based on the UTC timezone, do for example [DateTimeFormat(date:now(),timezone:'UTC')].");
+		
+		/*int offset = pc.getTimeZone().getOffset(date.getTime());
 		conversionType=conversionType.toLowerCase();
 		
 		if(conversionType.equals("local2utc")) {
@@ -20,6 +23,6 @@ public final class DateConvert implements Function {
 		else if(conversionType.equals("utc2local")) {
 			return new DateTimeImpl(pc,date.getTime()+offset,false);
 		}		
-		throw new ExpressionException("invalid conversion-type ["+conversionType+"] for function dateConvert");
+		throw new ExpressionException("invalid conversion-type ["+conversionType+"] for function dateConvert");*/
 	}
 }
