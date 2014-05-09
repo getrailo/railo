ec6977c533526a2db11cbe4f1062cbc47d3049b9
Igal <igal@getrailo.org>
2014-05-09 13:08:33 -0700

    allow to set config directory from command line

--- railo-java/railo-loader/src/railo/cli/CLI.java
+++ railo-java/railo-loader/src/railo/cli/CLI.java
@@ -12,7 +12,7 @@ import railo.loader.util.Util;
 
 public class CLI {
 	
-	private static boolean useRMI=false;
+	private static boolean useRMI;
 /**
  * Config
  * 
@@ -30,28 +30,39 @@ public class CLI {
 	 * @throws JspException 
 	 */
 	public static void main(String[] args) throws ServletException, IOException, JspException {
-		Map<String,String> config=toMap(args);
+
+		Map<String,String> config = toMap(args);
 		
 		System.setProperty("railo.cli.call", "true");
-		
-		// webroot
-		String strWebroot=config.get("webroot");
-		if(Util.isEmpty(strWebroot,true)) throw new IOException("missing webroot configuration");
-		File root=new File(strWebroot);
+
+		String param;
+
+		param = config.get("rmi");
+		if (param != null && param.equalsIgnoreCase("true"))
+			useRMI = true;
+
+		param = config.get("webroot");
+		if (Util.isEmpty(param,true))   throw new IOException("missing webroot configuration");
+		File root = new File(param);
 		root.mkdirs();
-		
-		// servletNane
-		String servletName=config.get("servlet-name");
-		if(Util.isEmpty(servletName,true))servletName="CFMLServlet";
-		if(useRMI){
-			CLIFactory factory = new CLIFactory(root,servletName,config);
+
+		String servletName = config.get("servlet-name");
+
+		if (Util.isEmpty(servletName, true))
+			servletName = "CFMLServlet";
+
+		if (useRMI) {
+
+			CLIFactory factory = new CLIFactory(root, servletName, config);
 			factory.setDaemon(false);
 			factory.start();
 		}
 		else {
+
 			CLIInvokerImpl invoker=new CLIInvokerImpl(root, servletName);
 			invoker.invoke(config);
 		}
+
 		//Map<String,Object> attributes=new HashMap<String, Object>();
 		//Map<String, String> initParameters=new HashMap<String, String>();
 		//initParameters.put("railo-server-directory", new File(root,"server").getAbsolutePath());
@@ -61,30 +72,42 @@ public class CLI {
 		//ServletConfigImpl servletConfig = new ServletConfigImpl(servletContext, servletName);
 		//CFMLEngine engine = CFMLEngineFactory.getInstance(servletConfig);
 		//engine.cli(config,servletConfig);
-		
-
 	}
 // java railo-cli.jar -config=.../railo-web.xml.cfm -uri=/susi/index.cfm?test=1 -form=name=susi -cgi=user_agent=urs -output=.../test.txt ...
-
+// java -Drailo.cli.config=F:\test\railo-cli-config -cp %CLASSPATH% railo.cli.CLI -webroot=. -uri=/time.cfm
+	
 	private static Map<String, String> toMap(String[] args) {
+
 		int index;
-		Map<String, String> config=new HashMap<String, String>();
 		String raw,key,value;
-		if(args!=null)for(int i=0;i<args.length;i++){
-			raw=args[i].trim();
-			if(Util.isEmpty(raw, true)) continue;
-			if(raw.startsWith("-"))raw=raw.substring(1).trim();
-			index=raw.indexOf('=');
-			if(index==-1) {
-				key=raw;
-				value="";
+
+		Map<String, String> config = new HashMap<String, String>();
+
+		if (args != null && args.length > 0) {
+
+			for (int i=0; i<args.length; i++) {
+
+				raw = args[i].trim();
+				if (raw.startsWith("-"))
+					raw = raw.substring(1);
+
+				if (!raw.isEmpty()) {
+
+					index = raw.indexOf('=');
+					if (index == -1) {
+						key = raw;
+						value = "";
+					}
+					else {
+						key = raw.substring(0, index).trim();
+						value = raw.substring(index + 1).trim();
+					}
+
+					config.put(key.toLowerCase(), value);
+				}
 			}
-			else {
-				key=raw.substring(0,index).trim();
-				value=raw.substring(index+1).trim();
-			}
-			config.put(key.toLowerCase(), value);
 		}
+
 		return config;
 	}
 }
