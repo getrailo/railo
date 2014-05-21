99ff08ae483fdb4ce3fdde33f7737b92851bd32f
micstriit <michael@getrailo.com>
2014-05-21 16:27:52 +0200

    solved ticket https://issues.jboss.org/browse/RAILO-3077

--- railo-java/railo-core/src/railo/runtime/functions/dynamicEvaluation/DE.java
+++ railo-java/railo-core/src/railo/runtime/functions/dynamicEvaluation/DE.java
@@ -9,7 +9,7 @@ import railo.runtime.ext.function.Function;
 import railo.runtime.functions.string.Replace;
 
 public final class DE implements Function {
-	public static String call(PageContext pc , String string) throws ExpressionException {
+	public static String call(PageContext pc , String string) {
 		return new StringBuilder().append('"').append(Replace.call(pc,string,"\"","\"\"","all")).append('"').toString();
 		
 	}
