d4eb464a9e767d099568df698ba2be8b69b1a0ff
Igal <igal@getrailo.org>
2014-05-27 11:12:09 -0700

    solve ticket https://issues.jboss.org/browse/RAILO-3091

--- railo-java/railo-core/src/railo/runtime/tag/CFTag.java
+++ railo-java/railo-core/src/railo/runtime/tag/CFTag.java
@@ -326,6 +326,9 @@ public class CFTag extends BodyTagTryCatchFinallyImpl implements DynamicAttribut
         try {
 			cfc = ComponentLoader.loadComponent(pageContext,null,source.getPageSource(), source.getFilename().substring(0,source.getFilename().length()-(pageContext.getConfig().getCFCExtension().length()+1)), false,true);
 		}
+        catch (TemplateException te) {
+	        throw te;
+        }
 		catch (PageException e) {
 			Mapping m = source.getPageSource().getMapping();
 			//Physical:/Users/mic/Projects/Railo/work/context/library/tag;
